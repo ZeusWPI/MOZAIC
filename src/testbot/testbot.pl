7b1389856c594f94f79b9d1759290cc50963b46a
Str-Gen <laurens.dhooge@telenet.be>
2017-07-05 20:58:32 +0200

    testbot

---
+++ src/testbot/testbot.pl
@@ -0,0 +1,17 @@
+use warnings;
+use strict;
+use JSON::Tiny qw(decode_json encode_json);
+use Data::Dumper;
+
+my %gamestate = ();
+my $gamestate_ref = \%gamestate;
+
+{
+  undef $/;
+  my $content = <>;
+  $gamestate_ref = decode_json $content;
+
+}
+
+print Dumper($gamestate_ref);
+#$$gamestate_ref{"foo"} = "bara";
