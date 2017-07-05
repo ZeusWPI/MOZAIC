f19fed5ce56d4545c5bd86e1690f6b8a4ecbbfd3
Ilion Beyst <ilion.beyst@gmail.com>
2017-07-06 01:55:04 +0200

    use refcells

--- src/supervisor_maar_wout_wil_liever_bot_driver/src/planetwars.rs
+++ src/supervisor_maar_wout_wil_liever_bot_driver/src/planetwars.rs
@@ -1,45 +1,27 @@
 use std::collections::HashMap;
+
 use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 
 use protocol::*;
 
-pub struct InitialStateBuilder {
-    state: State
-}
-
-impl InitialStateBuilder {
-    pub fn add_player(&mut self, player: String) {
-        self.state.players.push(player);
-    }
-
-    pub fn add_planet(&mut self, name: String, x: f64, y: f64, ship_count: u64, owner: Option<String>) {
-        self.state.planets.push(Planet {
-            ship_count: ship_count,
-            x: x,
-            y: y,
-            owner: owner,
-            name: name,
-        });
-    }
-}
-
 struct PlanetWars {
-    players: HashMap<String, Rc<Player>>,
-    planets: HashMap<String, Rc<Planet>>,
+    players: HashMap<String, Rc<RefCell<Player>>>,
+    planets: HashMap<String, Rc<RefCell<Planet>>>,
     expeditions: Vec<Expedition>,
 }
 
 struct Planet {
     name: String,
-    owner: Weak<Player>,
+    owner: Option<Weak<RefCell<Player>>>,
     ship_count: u64,
     x: f64,
     y: f64,
 }
 
 struct Expedition {
-    target: Weak<Planet>,
-    owner: Weak<Player>,
+    target: Weak<RefCell<Planet>>,
+    owner: Weak<RefCell<Player>>,
     ship_count: u64,
     turns_remaining: u64,
 }
