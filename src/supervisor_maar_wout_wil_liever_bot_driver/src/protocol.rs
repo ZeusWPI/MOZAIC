22a963418d9ca24dd1a08808f510717875109f79
Olivier de Schaetzen <olivier4@telenet.be>
2017-07-05 20:47:13 +0200

    changes to protocol

--- src/supervisor_maar_wout_wil_liever_bot_driver/src/protocol.rs
+++ src/supervisor_maar_wout_wil_liever_bot_driver/src/protocol.rs
@@ -1,12 +1,17 @@
-struct Game {
-}
-
-struct Fleet {
+struct Expedition {
     ship_count: u64,
+    origin: String,
+    destination: String,
+    owner: String,
+    turns_remaining: i64,
 }
 
 struct Planet {
     ship_count: u64,
+    x: f64,
+    y: f64,
+    owner: String,
+    name: String,
 }
 
 struct Player {
