94d8377211c85c3b16f8c5ce58351035c8da89c1
Str-Gen <laurens.dhooge@telenet.be>
2017-07-28 00:24:28 +0200

    Finished worldcreator

--- src/testbot/dummyworld/worldcreator.pl
+++ src/testbot/dummyworld/worldcreator.pl
@@ -1,30 +1,34 @@
 use strict;
 use warnings;
 use 5.014;
-use JSON::Tiny;
+use JSON::Tiny qw(encode_json);
 use Data::Dumper;
 
+if($#ARGV+1 != 5 || $ARGV[0] > $ARGV[1]){
+  say "Usage:
+  worldcreator.pl [playercount] [planetcount] [planets owned at start] [X map bound] [Y map bound]";
+  die;
+}
+say "::: Worldcreator for planetwars :::";
 my $playercount = shift @ARGV;
 my $planetcount = shift @ARGV;
 my $owned_on_start = shift @ARGV;
 my $x_bound = shift @ARGV;
 my $y_bound = shift @ARGV;
 
-if($#ARGV != 5){
-  say "Usage:\n
-  worldcreator.pl [playercount] [planetcount] [planets owned at start] [X map bound] [Y map bound]";
-}
+# This program creates a world for the planetwars game
+# overlap in names / coordinates has been made impossible
 
 use constant PLAYER_NAME_LENGTH => 8;
 use constant PLANET_NAME_LENGTH => 10;
-
-
 
 my %world = (
 "players" => [],
 "planets" => [],
 "expeditions" => []
 );
+
+say "::: Generating unique players :::";
 
 my %players = ();
 my $i = chr(int(rand 26)+ord('A'));
@@ -42,15 +46,17 @@ while(scalar keys %players != $playercount){
 
 @{$world{"players"}} = keys %players;
 
-print Dumper(\%world);
+#print Dumper(\%world);
 
 my $planet = {
   "x"=>0,
   "y"=>0,
   "ship_count"=>0,
   "owner"=>"",
-  "name="=>""
+  "name"=>""
 };
+
+say "::: Generating unique planets :::";
 
 my @planets = ();
 $i = chr(int(rand 26)+ord('A'));
@@ -65,23 +71,46 @@ while (scalar @planets != $planetcount){
     # X & Y
     my $X_choice = rand $#X;
     my $Y_choice = rand $#Y;
-    $Xplanet = $X[$X_choice];
+    $Xplanet = int($X[$X_choice]);
+    $Yplanet = int($Y[$Y_choice]);
+    $$planet{"x"} = $Xplanet;
+    $$planet{"y"} = $Yplanet;
     splice(@X,$X_choice,1);
-    $Yplanet = $Y[$Y_choice];
     splice(@Y,$Y_choice,1);
-    $$planet{"x"} = $X_choice;
-    $$planet{"y"} = $Y_choice;
     # A planet name
     for my $k (1..PLANET_NAME_LENGTH){
       $planetname .= $i;
       $i = chr(int(rand 26)+ord('A'));
     }
     $$planet{"name"} = $planetname;
+    $$planet{"ship_count"} = 0;
+    $$planet{"owner"}="";
     push @planets,$planet;
     $planetname = "";
+    $planet = {};
   }
 }
 
-print Dumper(\@planets);
+$i = 0;
+for my $k (@planets[0..$playercount-1]){
+  ${$k}{"owner"}=(keys %players)[$i];
+  ${$k}{"ship_count"}=$owned_on_start;
+  $i++;
+}
+#print Dumper(\@planets);
 
-#for my $j (1..$planetcount)
+@{$world{"planets"}} = @planets;
+
+#print Dumper(\%world);
+
+my $encoded = encode_json(\%world);
+#say $encoded;
+say "::: Encoded to JSON :::";
+
+my $result = `mktemp -p .`;
+chomp($result);
+
+open(my $fh,'>',$result);
+print $fh "$encoded";
+
+say "::: Finished writing this map to $result :::";
