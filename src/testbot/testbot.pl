53c59dafb799e0901174a68eaa2a38f0ea4bafc5
Str-Gen <laurens.dhooge@telenet.be>
2017-07-27 18:21:01 +0200

    finish testbot

--- src/testbot/testbot.pl
+++ src/testbot/testbot.pl
@@ -23,33 +23,60 @@ my @planets = @{$$gamestate_ref{"planets"}};
 my @expeditions = @{$$gamestate_ref{"expeditions"}};
 
 my $whoami = $players[rand $#players];
-say "Hi I will be playing as $whoami";
+say "[*] Hi I will be playing as $whoami";
 
 my %ownership_planets = map {
   my $playername = $_;
-  
   my @owned = grep {$$_{"owner"} eq $playername} @planets;
   #print Dumper(\@owned);
   $_=>\@owned;
-  
 } @players;
-
 #print Dumper(\%ownership_planets);
 
-
 my $whoami_planets = $ownership_planets{$whoami};
-say $whoami_planets;
 my $move_origin_planet = ${$whoami_planets}[rand $#{$whoami_planets}];
+say "[$whoami] I will attack from the following planet";
 print Dumper($move_origin_planet);
-my $move_ship_count = floor(${$move_origin_planet}{"ship_count"}/2);
+my $move_ship_count = ceil(${$move_origin_planet}{"ship_count"}/2);
+
+#decrement the ship_count on the planet you are leaving
+${$move_origin_planet}{"ship_count"} -= $move_ship_count;
 
 
 my @valid_targets = ( grep { $$_{"name"} ne ${$move_origin_planet}{"name"}} @planets);
 #print Dumper(\@valid_targets);
 
 my $valid_target = $valid_targets[rand $#valid_targets];
+say "[$whoami] I have selected this target";
 print Dumper($valid_target);
 
+my $command = {
+  "ship_count" => $move_ship_count,
+  "origin" => ${$move_origin_planet}{"name"},
+  "destination" => ${$valid_target}{"name"},
+};
+
+my $command_json = encode_json($command);
+
+say "[$whoami] I produced the following command: $command_json";
+
+
+push @expeditions ,{
+  "ship_count" => $move_ship_count,
+  "origin" => ${$move_origin_planet}{"name"},
+  "destination" => ${$valid_target}{"name"},
+  "owner" => $whoami,
+  "turns_remaining" => ceil(
+  Dist(
+  ${$move_origin_planet}{"x"},
+  ${$move_origin_planet}{"y"},
+  ${$valid_target}{"x"},
+  ${$valid_target}{"y"}
+  )
+  )
+};
+#print Dumper(\@expeditions);
+say "[$whoami] I pushed this new expedition";
 
 
 my %ownership_expeditions = map {
@@ -58,13 +85,10 @@ my %ownership_expeditions = map {
   $_=>\@owned;
 } @players;
 
-#print Dumper(\%ownership_expeditions);
-
-my $res = Dist((0,0,2,1));
-say ceil($res);
 
 sub Dist {
   my @P1 = (@_[0,1]);
   my @P2 = (@_[2,3]);
-  return sqrt(($P1[0]-$P2[0])*($P1[0]-$P2[0])+($P1[0]-$P2[0])*($P1[0]-$P2[0]));
+  #print Dumper(\@P1,\@P2);
+  return sqrt(($P1[0]-$P2[0])*($P1[0]-$P2[0])+($P1[1]-$P2[1])*($P1[1]-$P2[1]));
 }
