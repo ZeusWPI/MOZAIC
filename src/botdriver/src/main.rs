ad7d07cef1740e826d0c9fef2abb9d4e77b090c5
Wout Schellaert <wout.schellaert@gmail.com>
2017-08-09 20:45:28 +0200

    Split util function away from main

--- src/botdriver/src/main.rs
+++ src/botdriver/src/main.rs
@@ -1,6 +1,7 @@
 mod game_types;
 mod driver_types;
 mod higher_lower;
+mod util;
 
 #[macro_use]
 extern crate serde_derive;
@@ -8,21 +9,14 @@ extern crate serde_json;
 extern crate serde;
 extern crate rand;
 
-use std::error::Error;
-use std::env;
-use std::fs::File;
-use std::process::{Command, Stdio};
-use std::io::{Write, Read, BufReader, BufRead};
-use std::path::PathBuf;
-use std::collections::HashMap;
-
+use std::io::{Write, BufReader, BufRead};
 
 use game_types::{Game, GameInfo, GameStatus, Player, PlayerInput, PlayerOutput, PlayerCommand};
-use driver_types::{BotHandles, BotHandle, GameConfig, GameConfigFormat};
+use driver_types::{BotHandles, BotHandle, GameConfig};
 use higher_lower::HigherLower;
 
 fn main() {
-    let game_config: GameConfig = match parse_config() {
+    let game_config: GameConfig = match util::parse_config() {
         Ok(config) => config,
         Err(e) => {
             println!("{}", e);
@@ -32,40 +26,11 @@ fn main() {
     run::<HigherLower>(&game_config);
 }
 
-fn parse_config() -> Result<GameConfig, Box<Error>> {
-    let args: Vec<_> = env::args().collect();
-    if (args.len() < 2) || (args.len() > 2) {
-        let msg = format!("Expected 1 argument (config file). {} given.", args.len() - 1).to_owned();
-        return Err(From::from(msg))
-    }
-    
-    println!("Opening config {}", &args[1]);
-    let path = PathBuf::from(&args[1]);
-    let mut file = File::open(path)?;
-
-    println!("Reading contents");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    
-    println!("Parsing config");
-    let gcf: GameConfigFormat = serde_json::from_str(&contents)?;
-    let mut gc: GameConfig = GameConfig { players: HashMap::new() };
-    
-    // Move into easier to use struct
-    // TODO, make oneliner
-    for pc in gcf.players { 
-        gc.players.insert(pc.name.clone(), pc);
-    }
-    println!("Config parsed succesfully");
-
-    Ok(gc)
-}
-
 /* The algorithm for running a game goes as follows:
  *
  * ```
  * generate initial game
- * While (not finnished) do 
+ * while (not finnished) do 
  *   for each player
  *     send gamestate to player
  *     receive new commands
@@ -75,7 +40,7 @@ fn parse_config() -> Result<GameConfig, Box<Error>> {
 fn run<G: Game>(config: &GameConfig) {
     let mut game = G::init(config.players.keys().cloned().collect());
     let mut gamestate = game.start();
-    let mut handles = create_bot_handles(config);
+    let mut handles = util::create_bot_handles(config);
     loop {
         println!("\nNew step:\n==============");
         match gamestate {
@@ -95,25 +60,6 @@ fn run<G: Game>(config: &GameConfig) {
             } 
         }
     }
-}
-
-fn create_bot_handles(config: &GameConfig) -> BotHandles {
-    let mut children = BotHandles::new();
-
-    for (player, pconfig) in config.players.iter() {
-        let ref cmd = pconfig.start_command;
-        let bot = Command::new("bash")
-            .arg("-c")
-            .arg(format!("{} {}", cmd, player))
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("[DRIVER] Failed to execute process");
-
-        children.insert(player.clone(), bot);
-    }
-
-    children
 }
 
 // TODO: This could be prettier i guess
---
+++ src/botdriver/src/util.rs
@@ -0,0 +1,59 @@
+use serde_json;
+
+use std::error::Error;
+use std::path::PathBuf;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::process::{Command, Stdio};
+
+use driver_types::{GameConfig, GameConfigFormat, BotHandles};
+
+pub fn parse_config() -> Result<GameConfig, Box<Error>> {
+    let args: Vec<_> = env::args().collect();
+    if (args.len() < 2) || (args.len() > 2) {
+        let msg = format!("Expected 1 argument (config file). {} given.", args.len() - 1).to_owned();
+        return Err(From::from(msg))
+    }
+    
+    println!("Opening config {}", &args[1]);
+    let path = PathBuf::from(&args[1]);
+    let mut file = File::open(path)?;
+
+    println!("Reading contents");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    
+    println!("Parsing config");
+    let gcf: GameConfigFormat = serde_json::from_str(&contents)?;
+    let mut gc: GameConfig = GameConfig { players: HashMap::new() };
+    
+    // Move into easier to use struct
+    // TODO: make oneliner
+    for pc in gcf.players { 
+        gc.players.insert(pc.name.clone(), pc);
+    }
+    println!("Config parsed succesfully");
+
+    Ok(gc)
+}
+
+pub fn create_bot_handles(config: &GameConfig) -> BotHandles {
+    let mut children = BotHandles::new();
+
+    for (player, pconfig) in config.players.iter() {
+        let ref cmd = pconfig.start_command;
+        let bot = Command::new("bash")
+            .arg("-c")
+            .arg(format!("{} {}", cmd, player))
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("[DRIVER] Failed to execute process");
+
+        children.insert(player.clone(), bot);
+    }
+
+    children
+}
