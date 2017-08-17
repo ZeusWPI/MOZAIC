acc1ea38a04b83171af0e446ca21b1b5ae0b3235
tleilaxu <kenneth0@live.be>
2017-08-18 00:19:21 +0200

    JSON parsing

--- src/visualize/visualizer.js
+++ src/visualize/visualizer.js
@@ -1,122 +1,15 @@
-const data = {
-  "players": [
-    "Lorin",
-    "Tim",
-    "Iepoev"
-  ],
-  "planets": [{
-      "x": 10,
-      "y": 10,
-      "ship_count": 30,
-      "owner": "Lorin",
-      "name": "Mercurius"
-    },
-    {
-      "x": 10,
-      "y": 40,
-      "ship_count": 20,
-      "owner": "Tim",
-      "name": "Venus"
-    },
-    {
-      "x": 30,
-      "y": 25,
-      "ship_count": 20,
-      "owner": "Lorin",
-      "name": "Mars"
-    },
-    {
-      "x": 40,
-      "y": 12,
-      "ship_count": 20,
-      "owner": "Iepoev",
-      "name": "Pluto"
-    }
-  ],
-  "expeditions": [{
-      "ship_count": 10,
-      "origin": "Mercurius",
-      "destination": "Venus",
-      "owner": "Tim",
-      "turns_remaining": 11,
-    },
-    {
-      "ship_count": 10,
-      "origin": "Mars",
-      "destination": "Mercurius",
-      "owner": "Lorin",
-      "turns_remaining": 15,
-    },
-    {
-      "ship_count": 10,
-      "origin": "Mars",
-      "destination": "Venus",
-      "owner": "Lorin",
-      "turns_remaining": 7,
-    }
-  ]
-}
+// Constants
 
 const svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
-
-
-// Define patterns
 const planet_types = ["water", "red", "moon", "mars", "earth"];
-planet_types.forEach(p => {
-  svg.append("defs")
-    .append("pattern")
-    .attr("id", p)
-    .attr("viewBox", "0 0 100 100")
-    .attr("preserveAspectRation", "none")
-    .attr("width", 1)
-    .attr("height", 1)
-    .append("image")
-    .attr("width", 100)
-    .attr("height", 100)
-    .attr("preserveAspectRation", "none")
-    .attr("xlink:href", p + ".png");
-});
 
-svg.select("defs")
-  .append("pattern")
-  .attr("id", "ship")
-  .attr("viewBox", "0 0 100 100")
-  .attr("preserveAspectRation", "none")
-  .attr("width", 1)
-  .attr("height", 1)
-  .append("image")
-  .attr("width", 100)
-  .attr("height", 100)
-  .attr("preserveAspectRation", "none")
-  .attr("xlink:href", "rocket.png");
-
-fleets = [];
+// Help functions
 
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-const color_map = data.players.reduce((map, o, i) => {
-  map[o] = color(i);
-  return map;
-}, {});
-
-const planet_map = data.planets.reduce((map, o) => {
-  o.type = planet_types[Math.floor(Math.random() * planet_types.length)];
-  o.size = randomBetween(20, 60);
-  map[o.name] = o;
-  return map;
-}, {});
-
-data.expeditions.map((e) => {
-  e.origin = planet_map[e.origin];
-  e.destination = planet_map[e.destination];
-});
-
-// Help functions
 
 function euclideanDistance(e1, e2) {
   return Math.sqrt(Math.pow(e1.x - e2.x, 2) + Math.pow(e1.y - e2.y, 2));
@@ -140,25 +33,79 @@ function relativeCoords(expedition) {
   };
 }
 
-// Info
+function setupPatterns(svg) {
+  // Define patterns
+  planet_types.forEach(p => {
+    svg.append("defs")
+      .append("pattern")
+      .attr("id", p)
+      .attr("viewBox", "0 0 100 100")
+      .attr("preserveAspectRation", "none")
+      .attr("width", 1)
+      .attr("height", 1)
+      .append("image")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("preserveAspectRation", "none")
+      .attr("xlink:href", p + ".png");
+  });
 
-d3.select("body")
-  .selectAll("p")
-  .data(data.players)
-  .enter().append("p")
-  .text((d, i) => `I’m called ${d}!`)
-  .style('color', (d, i) => color(i));
+  svg.select("defs")
+    .append("pattern")
+    .attr("id", "ship")
+    .attr("viewBox", "0 0 100 100")
+    .attr("preserveAspectRation", "none")
+    .attr("width", 1)
+    .attr("height", 1)
+    .append("image")
+    .attr("width", 100)
+    .attr("height", 100)
+    .attr("preserveAspectRation", "none")
+    .attr("xlink:href", "rocket.png");
+}
 
+function prepareData(data) {
+  data.fleets = [];
+
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  data.color_map = data.players.reduce((map, o, i) => {
+    map[o] = color(i);
+    return map;
+  }, {});
+
+  data.planet_map = data.planets.reduce((map, o) => {
+    o.type = planet_types[Math.floor(Math.random() * planet_types.length)];
+    o.size = randomBetween(20, 60);
+    map[o.name] = o;
+    return map;
+  }, {});
+
+  data.expeditions.map((e) => {
+    e.origin = data.planet_map[e.origin];
+    e.destination = data.planet_map[e.destination];
+  });
+}
+
+function generateLegend(data) {
+  // Info
+  //TODO FIX
+  d3.select("body")
+    .selectAll("p")
+    .data(data.players)
+    .enter().append("p")
+    .text((d, i) => `I’m called ${d}!`)
+    .style('color', (d, i) => color(i));
+
+}
 // Rendering
 
-function renderOrbit(fleet, target) {
-  console.log(target);
+function renderOrbit(fleets, fleet, target) {
   time = Date.now();
   svg.append('circle')
     .attr('transform', 'translate(' + target.x * 20 + ',' + target.y * 20 + ')')
     .attr('r', fleet.distance)
     .style('fill', "none")
-    .style('stroke', color_map[target.owner]);
+    .style('stroke', fleet.color);
 
   let wrapper = svg.append('g')
     .attr('transform', 'translate(' + target.x * 20 + ',' + target.y * 20 + ')');
@@ -181,66 +128,81 @@ function renderOrbit(fleet, target) {
   d3.timer(renderFrame);
 }
 
-// Planets
+function generateMap(data) {
+  // Planets
 
-let planets = svg.selectAll('.planet')
-  .data(data.planets)
-  .enter();
+  let planets = svg.selectAll('.planet')
+    .data(data.planets)
+    .enter();
 
-planets.append('circle')
-  .attr("r", d => d.size)
-  .attr('class', 'planet')
-  .attr('cx', d => d.x * 20)
-  .attr('cy', d => d.y * 20)
-  .attr('fill', d => "url(#" + d.type + ")");
+  planets.append('circle')
+    .attr("r", d => d.size)
+    .attr('class', 'planet')
+    .attr('cx', d => d.x * 20)
+    .attr('cy', d => d.y * 20)
+    .attr('fill', d => "url(#" + d.type + ")");
 
-planets.append('text')
-  .attr('x', d => d.x * 20)
-  .attr('y', d => d.y * 20 + d.size + 20)
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "20px")
-  .attr('fill', d => color_map[d.owner])
-  .text(d => d.name);
+  planets.append('text')
+    .attr('x', d => d.x * 20)
+    .attr('y', d => d.y * 20 + d.size + 20)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "20px")
+    .attr('fill', d => data.color_map[d.owner])
+    .text(d => d.name);
 
-planets.append('text')
-  .attr('x', d => d.x * 20)
-  .attr('y', d => d.y * 20 + d.size + 60)
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "20px")
-  .attr('fill', d => color_map[d.owner])
-  .text(d => "\u2694 " + d.ship_count);
+  planets.append('text')
+    .attr('x', d => d.x * 20)
+    .attr('y', d => d.y * 20 + d.size + 60)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "20px")
+    .attr('fill', d => data.color_map[d.owner])
+    .text(d => "\u2694 " + d.ship_count);
 
-// Expeditions
+  // Expeditions
 
-let expeditions = svg.selectAll('.expedition')
-  .data(data.expeditions)
-  .enter().append('g')
-  .attr('transform', d => 'translate(' + relativeCoords(d).x * 20 + ',' + relativeCoords(d).y * 20 + ')');
-expeditions.append('circle')
-  .attr('transform', (d, i) => {
-    return 'rotate(' + (Math.atan2(d.destination.y - d.origin.y, d.destination.x - d.origin.x) * (180 / Math.PI) + 90) + ')';
-  })
-  .attr('r', 20)
-  .style('stroke', d => color_map[d.owner])
-  .style('stroke-width', 2)
-  .attr('fill', d => "url(#ship)");
+  let expeditions = svg.selectAll('.expedition')
+    .data(data.expeditions)
+    .enter().append('g')
+    .attr('transform', d => 'translate(' + relativeCoords(d).x * 20 + ',' + relativeCoords(d).y * 20 + ')');
+  expeditions.append('circle')
+    .attr('transform', (d, i) => {
+      return 'rotate(' + (Math.atan2(d.destination.y - d.origin.y, d.destination.x - d.origin.x) * (180 / Math.PI) + 90) + ')';
+    })
+    .attr('r', 20)
+    .style('stroke', d => data.color_map[d.owner])
+    .style('stroke-width', 2)
+    .attr('fill', d => "url(#ship)");
 
-expeditions.append('text')
-  .attr('y', 40)
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "20px")
-  .attr('fill', d => color_map[d.owner])
-  .text(d => "\u2694 " + d.ship_count);
+  expeditions.append('text')
+    .attr('y', 40)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "20px")
+    .attr('fill', d => data.color_map[d.owner])
+    .text(d => "\u2694 " + d.ship_count);
 
-svg.selectAll('.planet').each(d => {
-  fleet = {
-    size: 20,
-    distance: d.size + 40,
-    angle: randomBetween(1, 360),
-    speed: randomBetween(100, 1000),
-    ship_count: d.ship_count,
-    owner: d.owner
-  };
-  fleets.push(fleet);
-  renderOrbit(fleet, d);
-});
+  svg.selectAll('.planet').each(d => {
+    fleet = {
+      size: 20,
+      distance: d.size + 40,
+      angle: randomBetween(1, 360),
+      speed: randomBetween(100, 1000),
+      ship_count: d.ship_count,
+      owner: d.owner,
+      color: data.color_map[d.owner]
+    };
+    data.fleets.push(fleet);
+    renderOrbit(data.fleets, fleet, d);
+  });
+}
+
+function parseJson(e) {
+  var reader = new FileReader();
+  reader.onload = event => {
+    var parsed = JSON.parse(event.target.result);
+    setupPatterns(svg);
+    var data = parsed.turns[0];
+    prepareData(data);
+    generateMap(data);
+  }
+  reader.readAsText(e.files[0]);
+}
