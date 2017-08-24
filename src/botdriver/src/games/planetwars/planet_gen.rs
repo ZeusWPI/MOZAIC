21a0a2b325bd86569ce44a9d5ce3502066427918
Wout Schellaert <wout.schellaert@gmail.com>
2017-08-24 16:20:15 +0200

    Fix planets on same location

--- src/botdriver/src/games/planetwars/planet_gen.rs
+++ src/botdriver/src/games/planetwars/planet_gen.rs
@@ -14,29 +14,33 @@ const HEIGHT: u64 = 25;
 const START_SHIPS: u64 = 15;
 const PLANET_FACTOR: u64 = 2;
 
-// TODO: Fix possible planets on same location
 pub fn gen_planets(players: Vec<PlayerName>) -> HashMap<PlanetName, Rc<RefCell<Planet>>> {
     let mut planets = HashMap::new();
-
-    for player in &players {
-        let planet_name = format!("{}_Base", player);
-        let planet = gen_planet(planet_name.clone(), player.clone());
-        planets.insert(planet_name, planet);
-    }
-
     let player_amount = players.len() as u64;
     let planet_amount = player_amount * PLANET_FACTOR;
-    for x in 0..(planet_amount - player_amount) {
+    let locations = gen_locations(planet_amount);
+
+    for (x, y) in locations {
         let planet_name = format!("Planet_{}", x);
-        let planet = gen_planet(planet_name.clone(), "".to_string());
+        let planet = Planet::new(
+            planet_name.to_string(),
+            Vec::new(),
+            x,
+            y
+        );
+        let planet = Rc::new(RefCell::new(planet));
         planets.insert(planet_name, planet);
     }
-    
     planets
 }
 
-fn gen_planet(planet_name: PlanetName, owner: PlayerName) -> Rc<RefCell<Planet>> {
-    let x = rand::thread_rng().gen_range(0, WIDTH);
-    let y = rand::thread_rng().gen_range(0, HEIGHT);
-    Rc::new(RefCell::new(Planet::new(planet_name.to_string(), Vec::new(), x, y)))
+// We do this so we can prevent planets being generated on the same location
+pub fn gen_locations(amount: u64) -> Vec<(u64, u64)> {
+    let mut v = (0..amount).map(|_| {
+        let x = rand::thread_rng().gen_range(0, WIDTH);
+        let y = rand::thread_rng().gen_range(0, HEIGHT);
+        (x, y)
+    }).collect::<Vec<(u64, u64)>>();
+    v.dedup();
+    v
 }
