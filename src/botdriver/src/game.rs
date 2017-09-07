7f0a7034f9a44e4288322898bd59d1983393932e
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-07 19:50:15 +0200

    add logging capability to game output

--- src/botdriver/src/game.rs
+++ src/botdriver/src/game.rs
@@ -1,10 +1,25 @@
 use std::collections::HashMap;
 
 pub trait Game {
+    type Config;
+
     // returns game state and initial status
-    fn init(player_ids: Vec<usize>) -> (Self, GameStatus);
+    fn init(config: Config, player_ids: Vec<usize>) -> (Self, GameOutput);
     // process player input and execute a game turn
-    fn step(&mut self, responses: &Vec<Response>) -> GameStatus;
+    fn step(&mut self, responses: &Vec<Response>) -> GameOutput;
+}
+
+#[derive(Debug)]
+pub struct GameOutput {
+    pub status: GameStatus,
+    pub log_entry: Option<String>,
+}
+
+// reason why the game returned control
+#[derive(Debug)]
+pub enum GameStatus {
+    Finished(Outcome),
+    Suspended(Vec<Prompt>)
 }
 
 #[derive(Debug)]
@@ -18,14 +33,6 @@ pub struct Response {
     pub player_id: usize,
     pub data: String,
 }
-
-// Reason why the game returned control.
-#[derive(Debug)]
-pub enum GameStatus {
-    Finished(Outcome),
-    Suspended(Vec<Prompt>)
-}
-
 // TODO Improve error type. error-chain maybe?
 #[derive(Debug)]
 pub enum Outcome {
