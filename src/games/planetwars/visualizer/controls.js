1221d27e95a51ac0f1f1e5cb4b4777e4c8be1b2c
tl3ilaxu <kenneth0@live.be>
2017-09-08 01:26:16 +0200

    Make object oriented

--- src/games/planetwars/visualizer/controls.js
+++ src/games/planetwars/visualizer/controls.js
@@ -1,42 +1,51 @@
-let mod = 3;
-var speeds = [0.25, 0.33, 0.5, 1, 2, 3, 4];
+class Controls {
+  constructor() {
+    this.mod = 3;
+    this.speeds = [0.25, 0.33, 0.5, 1, 2, 3, 4];
+  }
 
-function attachEvents(maxturn, togglePlaybackCallback, changeTurnCallback) {
-  d3.select('#toggleplay').on("click", e => {
-    var button = d3.select('#toggleplay');
-    if (togglePlaybackCallback()) {
-      button.text("\u23F8");
-    } else {
-      button.text("\u25B6");
-    }
-  });
-
-  d3.select('#speeddown').on("click", e => {
-    if (mod > 0) {
-      mod--;
-      speed = base_speed / speeds[mod];
-      updateSpeed(speeds[mod]);
-    }
-  })
-
-  d3.select('#speedup').on("click", e => {
-    if (mod < speeds.length - 1) {
-      mod++;
-      speed = base_speed / speeds[mod];
-      updateSpeed(speeds[mod]);
-    }
-  })
-
-  d3.select('#turn_slider')
-    .attr('min', 0)
-    .attr('max', maxturn)
-    .attr('step', 1)
-    .on('change', e => {
-      changeTurnCallback(d3.select('#turn_slider').node().value);
+  attachEvents(visualizer) {
+    d3.select('#toggleplay').on("click", e => {
+      var button = d3.select('#toggleplay');
+      if (visualizer.toggleTimer()) {
+        button.text("\u23F8");
+      } else {
+        button.text("\u25B6");
+      }
     });
-  updateSpeed(speeds[mod]);
+
+    d3.select('#speeddown').on("click", e => {
+      if (this.mod > 0) {
+        this.mod--;
+        visualizer.speed = base_speed / this.speeds[this.mod];
+        this.updateSpeed(this.speeds[this.mod]);
+      }
+    });
+
+    d3.select('#speedup').on("click", e => {
+      if (this.mod < this.speeds.length - 1) {
+        this.mod++;
+        visualizer.speed = base_speed / this.speeds[this.mod];
+        this.updateSpeed(this.speeds[this.mod]);
+      }
+    });
+
+    d3.select('#turn_slider')
+      .attr('min', 0)
+      .attr('max', visualizer.maxTurns)
+      .attr('step', 1)
+      .on('change', e => {
+        visualizer.showTurn(d3.select('#turn_slider').node().value);
+      });
+    this.updateSpeed(this.speeds[this.mod]);
+
+  }
+
+  updateSpeed(val) {
+    d3.select('.speed').text("Speed x" + val);
+  }
 }
 
-function updateSpeed(val) {
-  d3.select('.speed').text("Speed x" + val);
-}
+
+var controls = new Controls();
+var visualizer = new Visualizer(controls);
