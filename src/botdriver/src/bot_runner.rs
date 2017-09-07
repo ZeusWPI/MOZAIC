88001a5571870de2c32a2331e9d0851c74bcc968
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-07 21:47:19 +0200

    implement players struct

--- src/botdriver/src/bot_runner.rs
+++ src/botdriver/src/bot_runner.rs
@@ -23,16 +23,49 @@ pub struct Players {
 
 impl Players {
     pub fn start(configs: &Vec<PlayerConfig>) -> Self {
-        unimplemented!()
+        let mut handles = HashMap::with_capacity(configs.len());
+        for player_config in configs.iter() {
+            let handle = process::Command::new(player_config.command)
+                .args(player_config.args)
+                .arg(player_config.id)
+                .stdin(process::Stdio::piped())
+                .stdout(process::Stdio::piped())
+                .spawn()
+                .expect(&format!("\n[DRIVER] Failed to execute process: {} {:?}\n", cmd, args));
+            handles.insert(player_config.id, handle);
+        }
+        return Players { handles };
     }
 
     // Communicates prompts to the respective players, and aggregates their
     // responses.
-    pub fn handle_prompts(&mut self, &Vec<Prompt>) -> Vec<Response> {
-        unimplemented!()
+    pub fn handle_prompts(&self, prompts: &Vec<Prompt>) -> Vec<Response> {
+        self.send_prompts(prompts);
+        return self.read_responses(prompts);
+    }
+
+    fn send_prompts(&self, prompts: &Vec<Prompt>) {
+        for prompt in promts.iter() {
+            let player_in = self.handles[prompt.id].stdin.unwrap();
+            player_in.write_fmt(format_args!("{}\n", prompt.data))?;
+            player_in.flush();
+        }
+    }
+
+    fn read_responses(&self, prompts: &Vec<Prompt>) -> Vec<Response> {
+        prompts.iter().map(|prompt| {
+            let id = prompt.id;
+            let mut reader = BufReader::new(self.handles[id].stdout.unwrap());
+            let mut data = String::new();
+            // for now, panic on invalid output - we don't have error handling yet.
+            reader.read_line(&mut data).unwrap();
+            return Response { id, data };
+        }).collect()
     }
 
     pub fn kill(self) {
-        unimplemented!()
+        for handle in self.handles.values() {
+            handle.kill();
+        }
     }
 }
