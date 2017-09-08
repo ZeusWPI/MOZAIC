52ec136a66fe6d2c154696d8861aca4815dff757
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-08 15:26:44 +0200

    change game types again

--- src/botdriver/src/game.rs
+++ src/botdriver/src/game.rs
@@ -1,49 +1,25 @@
 use std::collections::HashMap;
 
-pub trait Game {
+pub trait Game : Sized {
     type Config;
 
     // returns game state and initial status
     fn init(config: Self::Config, player_ids: Vec<u64>) -> (Self, GameStatus)
         where Self: Sized;
     // process player input and execute a game turn
-    fn step(&mut self, responses: &Vec<Response>) -> GameStatus;
-}
-
-#[derive(Debug)]
-pub struct GameStatus {
-    pub control_status: ControlStatus,
-    pub log_entry: Option<String>,
+    fn step(&mut self, responses: &PlayerMap<String>) -> GameStatus;
 }
 
 // reason why the game returned control
 #[derive(Debug)]
-pub enum ControlStatus {
-    Finished(Outcome),
-    Prompting(Vec<Prompt>)
+pub enum GameStatus {
+    // map players to the prompt they should reply to
+    Prompting(PlayerMap<String>),
+    // map players to their score
+    Finished(PlayerMap<f64>),
 }
 
-#[derive(Debug)]
-pub struct Prompt {
-    pub player_id: u64,
-    pub data: String,
-}
-
-#[derive(Debug)]
-pub struct Response {
-    pub player_id: u64,
-    pub data: Option<String>,
-}
-
-// TODO Improve error type. error-chain maybe?
-#[derive(Debug)]
-pub enum Outcome {
-    Score(Vec<PlayerScore>),
-    Error(String),
-}
-
-#[derive(Debug, Clone)]
-pub struct PlayerScore {
-    pub player_id: u64,
-    pub score: f64,
-}
+// TODO: might be better to put this elsewhere
+// Maps a player to something.
+// TODO: non-cryptographic hash function maybe
+pub type PlayerMap<T> = HashMap<u64, T>;
