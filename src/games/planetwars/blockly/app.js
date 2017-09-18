65c9763923ad912b913fb4ff5e92622fe4ba162c
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-18 20:23:47 +0200

    get blockly working

--- src/games/planetwars/blockly/app.js
+++ src/games/planetwars/blockly/app.js
@@ -1,1 +1,8 @@
-document.write("it works")
+var Blockly = require("node-blockly/browser");
+
+window.onload = function() {
+  console.log("ok");
+  var workspaceplayground = Blockly.inject('blocklyDiv', {
+    toolbox: document.getElementById('toolbox')
+  });
+};
