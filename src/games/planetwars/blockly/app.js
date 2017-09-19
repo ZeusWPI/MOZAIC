84329e61b6016f6586bfa4dc8a370323b0048553
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-19 16:50:42 +0200

    add some builtin blocks to toolbox

--- src/games/planetwars/blockly/app.js
+++ src/games/planetwars/blockly/app.js
@@ -5,26 +5,63 @@ const blocks = require("./src/blocks");
 Blockly.HSV_SATURATION = 0.6;
 Blockly.HSV_VALUE = 0.8;
 
-// Register all blocks
-Object.values(blocks).forEach(cat => {
-  Object.entries(cat).forEach(([block_name, block]) => {
+
+var toolbox = {};
+
+
+// Register all custom blocks
+Object.entries(blocks).forEach(([cat_name, cat_blocks]) => {
+  var list = [];
+  Object.entries(cat_blocks).forEach(([block_name, block]) => {
     Blockly.Blocks[block_name] = block;
+    list.push(block_name);
   });
+  toolbox[cat_name] = list;
 });
+
+// Add builtins to toolbox
+toolbox['logic'] = [
+  'logic_boolean',
+  'logic_compare',
+  'logic_null',
+  'logic_operation',
+  'logic_ternary'
+];
+
+toolbox['variables'] = [
+  'variables_set',
+  'variables_get'
+];
+
+toolbox['math'] = [
+  'math_arithmetic',
+  'math_change',
+  'math_constant',
+  'math_constrain',
+  'math_modulo',
+  'math_number',
+  'math_number_property',
+  'math_on_list',
+  'math_random_float',
+  'math_random_int',
+  'math_round',
+  'math_single',
+  'math_trig'
+];
 
 // Bulid toolbox xml
-var toolbox = '<xml>';
-Object.entries(blocks).forEach(([cat_name, cat]) => {
-  toolbox += '<category name="' + cat_name + '">';
-  Object.keys(cat).forEach(block_name => {
-    toolbox += '<block type="' + block_name + '"></block>';
+var toolbox_str = '<xml>';
+Object.entries(toolbox).forEach(([cat_name, cat_entries]) => {
+  toolbox_str += '<category name="' + cat_name + '">';
+  cat_entries.forEach(block_name => {
+    toolbox_str += '<block type="' + block_name + '"></block>';
   });
-  toolbox += '</category>';
+  toolbox_str += '</category>';
 });
-toolbox += '</xml>';
+toolbox_str += '</xml>';
 
 window.onload = function() {
-  var workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
+  var workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox_str });
   workspace.addChangeListener(function() {
     var code = Blockly.JavaScript.workspaceToCode(workspace);
     document.getElementById('generatedCodeDiv').innerHTML = code;
