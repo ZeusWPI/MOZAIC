b895b28ee2b55704f24396e379f8c0458b13ed6f
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-19 14:46:50 +0200

    Dynamic toolbox. I feel so dirty.

--- src/games/planetwars/blockly/app.js
+++ src/games/planetwars/blockly/app.js
@@ -5,15 +5,20 @@ const blocks = require("./src/blocks");
 Blockly.HSV_SATURATION = 0.6;
 Blockly.HSV_VALUE = 0.8;
 
+// Register all blocks
 Object.entries(blocks).forEach(([name, block]) => {
   Blockly.Blocks[name] = block;
 });
 
-window.onload = function() {
-  var workspace = Blockly.inject('blocklyDiv', {
-    toolbox: document.getElementById('toolbox')
-  });
+// Bulid toolbox xml
+var toolbox = "<xml>";
+Object.keys(blocks).forEach(name => {
+  toolbox += "<block type=\"" + name + "\"></block>";
+});
+toolbox += "</xml>";
 
+window.onload = function() {
+  var workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
   workspace.addChangeListener(function() {
     var code = Blockly.JavaScript.workspaceToCode(workspace);
     document.getElementById('generatedCodeDiv').innerHTML = code;
