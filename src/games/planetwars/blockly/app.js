9b400998cfd3dad8e4bc9a445801167bbf0032f9
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-19 13:38:45 +0200

    get rid of some boilerplate

--- src/games/planetwars/blockly/app.js
+++ src/games/planetwars/blockly/app.js
@@ -1,9 +1,13 @@
 var Blockly = require("node-blockly/browser");
-require("./src/blocks");
+const blocks = require("./src/blocks");
 
 // happier colours
 Blockly.HSV_SATURATION = 0.6;
 Blockly.HSV_VALUE = 0.8;
+
+Object.entries(blocks).forEach(([name, block]) => {
+  Blockly.Blocks[name] = block;
+});
 
 window.onload = function() {
   var workspace = Blockly.inject('blocklyDiv', {
--- src/games/planetwars/blockly/src/blocks.js
+++ src/games/planetwars/blockly/src/blocks.js
@@ -2,45 +2,44 @@ var Blockly = require("node-blockly/browser");
 
 const MAGIC_OBJECT_COLOR = 275;
 
-Blockly.Blocks['planets'] = {
-  init: function() {
-    this.appendDummyInput().appendField('Planets');
-    this.setColour(MAGIC_OBJECT_COLOR);
-    this.setOutput(true, 'List');
+const blocks = {
+  'planets': {
+    init: function() {
+      this.appendDummyInput().appendField('Planets');
+      this.setColour(MAGIC_OBJECT_COLOR);
+      this.setOutput(true, 'List');
+    }
+  },
+  'expeditions': {
+    init: function() {
+      this.appendDummyInput().appendField('Expeditions');
+      this.setColour(MAGIC_OBJECT_COLOR);
+      this.setOutput(true, 'List');
+    }
+  },
+  'players': {
+    init: function() {
+      this.appendDummyInput().appendField('Players');
+      this.setColour(MAGIC_OBJECT_COLOR);
+      this.setOutput(true, 'List');
+    }
+  },
+  'attr_owner': {
+    init: function() {
+      this.appendValueInput('OBJ')
+        .setCheck(['Planet', 'Expedition'])
+        .appendField('owner of');
+      this.setOutput(true, 'Player');
+    }
+  },
+  'attr_ship_count': {
+    init: function() {
+      this.appendValueInput('OBJ')
+        .setCheck(['Planet', 'Expedition'])
+        .appendField('fleet size of');
+      this.setOutput(true, 'Number');
+    }
   }
 };
 
-Blockly.Blocks['expeditions'] = {
-  init: function() {
-    this.appendDummyInput().appendField('Expeditions');
-    this.setColour(MAGIC_OBJECT_COLOR);
-    this.setOutput(true, 'List');
-  }
-};
-
-Blockly.Blocks['players'] = {
-  init: function() {
-    this.appendDummyInput().appendField('Players');
-    this.setColour(MAGIC_OBJECT_COLOR);
-    this.setOutput(true, 'List');
-  }
-};
-
-
-Blockly.Blocks['get_owner'] = {
-  init: function() {
-    this.appendValueInput('OBJ')
-      .setCheck(['Planet', 'Expedition'])
-      .appendField('owner of');
-    this.setOutput(true, 'Player');
-  }
-};
-
-Blockly.Blocks['get_ship_count'] = {
-  init: function() {
-    this.appendValueInput('OBJ')
-      .setCheck(['Planet', 'Expedition'])
-      .appendField('fleet size of');
-    this.setOutput(true, 'Number');
-  }
-};
+module.exports = blocks;
