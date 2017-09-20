29fd29407d4aea9e4c8f06d00b66a580dfd39158
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-20 15:33:36 +0200

    add orders to functions

--- src/games/planetwars/blockly/src/js_gen/functions.js
+++ src/games/planetwars/blockly/src/js_gen/functions.js
@@ -1,36 +1,43 @@
-var Blockly = require('node-blockly/browser');
+const Blockly = require('node-blockly/browser');
+const JS = Blockly.JavaScript;
 
 module.exports = {
   'fn_owner': function(block) {
     var arg = Blockly.JavaScript.valueToCode(block, 'OBJ');
-    return arg + "['owner']";
+    var code = arg + "['owner']";
+    return [code, JS.ORDER_MEMBER];
   },
   'fn_ship_count': function(block) {
     var arg = Blockly.JavaScript.valueToCode(block, 'OBJ');
-    return arg + "['ship_count']";
+    var code = arg + "['ship_count']";
+    return [code, JS.ORDER_MEMBER];
   },
   'fn_origin': function(block) {
     var arg = Blockly.JavaScript.valueToCode(block, 'EXPEDITION');
-    return 'getPlanet(' + arg + "['origin'])";
+    var code = 'getPlanet(' + arg + "['origin'])";
+    return [code, JS.ORDER_FUNCTION_CALL];
   },
   'fn_target': function(block) {
     var arg = Blockly.JavaScirpt.valueToCode(block, 'EXPEDITION');
-    return 'getPlanet(' + arg + "['target'])";
+    var code = 'getPlanet(' + arg + "['target'])";
+    return [code, JS.ORDER_FUNCTION_CALL];
   },
   'fn_turns_remaining': function(block) {
     var arg = Blockly.JavaScript.valueToCode(block, 'EXPEDITION');
-    return arg + "['turns_remaining']";
+    var code = arg + "['turns_remaining']";
+    return [code, JS.ORDER_MEMBER];
   },
   'fn_distance': function(block) {
     var p1 = Blockly.JavaScript.valueToCode(block, 'PLANET1');
     var p2 = Blockly.JavaScript.valueToCode(block, 'PLANET2');
-    return 'distance(' + p1 + ', ' + p2 + ')';
+    var code = 'distance(' + p1 + ', ' + p2 + ')';
+    return [code, JS.ORDER_FUNCTION_CALL];
   },
   'fn_dispatch': function(block) {
     var num_ships = Blockly.JavaScript.valueToCode(block, 'SHIP_COUNT');
     var source = Blockly.JavaScript.valueToCode(block, 'SOURCE_PLANET');
     var target = Blockly.JavaScript.valueToCode(block, 'TARGET_PLANET');
-    return 'dispatch('+ num_ships + ', ' + source + ', ' + target + ')';
+    var code = 'dispatch('+ num_ships + ', ' + source + ', ' + target + ')';
+    return [code, JS.ORDER_FUNCTION_CALL];
   }
-  
 };
