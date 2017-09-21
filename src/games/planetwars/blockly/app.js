dc0ce3655691e967a6aa41da2179cbae7347c070
Ilion Beyst <ilion.beyst@gmail.com>
2017-09-21 12:39:31 +0200

    improved toolbox

--- src/games/planetwars/blockly/app.js
+++ src/games/planetwars/blockly/app.js
@@ -1,6 +1,5 @@
 var Blockly = require("node-blockly/browser");
-const blocks = require("./src/blocks");
-const js_gen = require("./src/js_gen");
+const Blocks = require("./src/blocks");
 
 const PlanetWars = require("./src/planetwars");
 
@@ -9,70 +8,74 @@ const eval_code = require("./src/test");
 // happier colours
 Blockly.HSV_SATURATION = 0.6;
 Blockly.HSV_VALUE = 0.8;
+Blocks.init();
 
-
-var toolbox = {};
-
-
-// Register all custom blocks
-Object.entries(blocks).forEach(([cat_name, cat_blocks]) => {
-  var toolbox_list = [];
-  Object.entries(cat_blocks).forEach(([block_name, block]) => {
-    Blockly.Blocks[block_name] = block;
-    toolbox_list.push(block_name);
-  });
-  toolbox[cat_name] = toolbox_list;
-});
-
-Object.entries(js_gen).forEach(([cat_name, cat_blocks]) => {
-  Object.entries(cat_blocks).forEach(([block_name, fn]) => {
-    Blockly.JavaScript[block_name] = fn;
-  });
-});
-
-// Add builtins to toolbox
-toolbox['logic'] = [
-  'logic_boolean',
-  'logic_compare',
-  'logic_null',
-  'logic_operation',
-  'logic_ternary'
-];
-
-toolbox['variables'] = [
-  'variables_set',
-  'variables_get'
-];
-
-toolbox['math'] = [
-  'math_arithmetic',
-  'math_change',
-  'math_constant',
-  'math_constrain',
-  'math_modulo',
-  'math_number',
-  'math_number_property',
-  'math_on_list',
-  'math_random_float',
-  'math_random_int',
-  'math_round',
-  'math_single',
-  'math_trig'
-];
+var toolbox = {
+  'entities': [
+    'entities_planets',
+    'entities_expeditions',
+    'entities_players',
+    'entities_player',
+    'entities_nobody'
+  ],
+  'functions': [
+    'functions_owner',
+    'functions_ship_count',
+    'functions_origin',
+    'functions_target',
+    'functions_turns_remaining',
+    'functions_distance',
+    'functions_dispatch'
+  ],
+  'lists': [
+    'lists_filter',
+    'lists_minmax'
+  ],
+  'logic': [
+    'logic_boolean',
+    'logic_compare',
+    'logic_null',
+    'logic_operation',
+    'logic_ternary'
+  ],
+  'variables': [
+    'variables_set',
+    'variables_get'
+  ],
+  'math': [
+    'math_arithmetic',
+    'math_change',
+    'math_constant',
+    'math_constrain',
+    'math_modulo',
+    'math_number',
+    'math_number_property',
+    'math_on_list',
+    'math_random_float',
+    'math_random_int',
+    'math_round',
+    'math_single',
+    'math_trig'
+  ]
+};
 
 // Bulid toolbox xml
-var toolbox_str = '<xml>';
-Object.entries(toolbox).forEach(([cat_name, cat_entries]) => {
-  toolbox_str += '<category name="' + cat_name + '">';
-  cat_entries.forEach(block_name => {
-    toolbox_str += '<block type="' + block_name + '"></block>';
+
+function toolbox_xml(toolbox) {
+  var toolbox_str = '<xml>';
+  Object.entries(toolbox).forEach(([cat_name, cat_entries]) => {
+    toolbox_str += '<category name="' + cat_name + '">';
+    cat_entries.forEach(block_name => {
+      toolbox_str += '<block type="' + block_name + '"></block>';
+    });
+    toolbox_str += '</category>';
   });
-  toolbox_str += '</category>';
-});
-toolbox_str += '</xml>';
+  toolbox_str += '</xml>';
+  return toolbox_str;
+}
 
 window.onload = function() {
-  var workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox_str });
+  var workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox_xml(toolbox) });
   workspace.addChangeListener(function() {
     var code = Blockly.JavaScript.workspaceToCode(workspace);
     document.getElementById('generatedCodeDiv').innerHTML = code;
