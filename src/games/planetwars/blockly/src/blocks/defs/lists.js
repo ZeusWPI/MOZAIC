af9e3d177424f1dbc93f13bd0dbed0fd82234ffb
Eloïse Piret <Eloise.Piret@ugent.be>
2017-09-22 16:54:52 +0200

    colourssssssssssss

--- src/games/planetwars/blockly/src/blocks/defs/lists.js
+++ src/games/planetwars/blockly/src/blocks/defs/lists.js
@@ -1,6 +1,6 @@
 var Blockly = require('node-blockly/browser');
 
-const LIST_COLOUR = Blockly.Blocks['lists'].HUE;
+const LIST_COLOUR = 189;//Blockly.Blocks['lists'].HUE;
 
 module.exports = {
     'filter': {
