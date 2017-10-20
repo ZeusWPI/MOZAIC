f4f616885ea33ca4f73f6013df2a557041d318c6
Ilion Beyst <ilion.beyst@gmail.com>
2017-10-20 19:27:29 +0200

    changes to BotHandle

--- src/botdriver/src/bot_runner.rs
+++ src/botdriver/src/bot_runner.rs
@@ -1,13 +1,10 @@
 use std::process;
-use std::io::{LineWriter, BufReader, Write};
 use futures::Future;
 use tokio_process::{Child, ChildStdin, ChildStdout, CommandExt};
 
-use tokio_io::io;
 use tokio_core::reactor::Handle;
 
-use std::io::Error as IoError;
-use std::io::Result;
+use std::io::{Read, Write, Error, Result};
 
 use game::*;
 use match_runner::*;
@@ -40,7 +37,8 @@ impl BotRunner {
 
     pub fn player_handles<'p>(&'p mut self) -> PlayerMap<PlayerHandle<'p>> {
         self.processes.iter_mut().map(|(&player_id, process)| {
-            (player_id, PlayerHandle::new(process))
+            //(player_id, PlayerHandle::new(process))
+            unimplemented!()
         }).collect()
     }
 
@@ -53,8 +51,8 @@ impl BotRunner {
 
 pub struct BotHandle {
     process: Child,
-    stdin: BufWriter<ChildStdin>,
-    stdout: BufReader<ChildStdout>,
+    stdin: ChildStdin,
+    stdout: ChildStdout,
 }
 
 impl Read for BotHandle {
@@ -74,34 +72,16 @@ impl Write for BotHandle {
 }
 
 impl BotHandle {
-    fn new(process: Child) -> BotHandle {
-        BotHandle [
+    fn new(mut process: Child) -> BotHandle {
+        BotHandle {
             stdin: process.stdin().take().unwrap(),
             stdout: process.stdout().take().unwrap(),
             process: process,
-        ]
+        }
     }
 }
+
 
 pub struct PlayerHandle<'p> {
     process: &'p mut Child
-}
-
-impl<'p> PlayerHandle<'p> {
-    pub fn new(process: &'p mut Child) -> Self {
-        PlayerHandle { process }
-    }
-
-    fn write_msg(&mut self, msg: &str) {
-        let stdin = self.process.stdin().as_mut().unwrap();
-        write!(stdin, "{}\n", msg);
-    }
-
-    pub fn prompt<'a>(&'a mut self, msg: &str) -> impl Future<Item = String, Error = IoError> + 'a {
-        self.write_msg(msg);
-        let stdout = self.process.stdout().as_mut().unwrap();
-        let reader = BufReader::new(stdout);
-        let bytes = io::read_until(reader, b'\n', Vec::new());
-        return bytes.map(|(_, vec)| String::from_utf8(vec).unwrap());
-    }
 }
