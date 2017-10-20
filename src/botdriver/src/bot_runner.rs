6e405291a54187396e27a7e3753ebdae968b8de3
Ilion Beyst <ilion.beyst@gmail.com>
2017-10-20 20:09:06 +0200

    expand BotHandle

--- src/botdriver/src/bot_runner.rs
+++ src/botdriver/src/bot_runner.rs
@@ -1,12 +1,12 @@
-use std::process;
-use futures::Future;
+use std::process::{Command, Stdio};
+use futures::{Future, Poll};
 use tokio_process::{Child, ChildStdin, ChildStdout, CommandExt};
 use tokio_io::codec::{Encoder, Decoder};
 
 use tokio_core::reactor::Handle;
 
 use std::io::{Read, Write, Error, ErrorKind, Result};
-use tokio_io::io;
+use tokio_io::{AsyncRead, AsyncWrite};
 
 use bytes::{BytesMut, BufMut};
 
@@ -25,18 +25,17 @@ impl BotRunner {
     pub fn run(handle: &Handle, players: &PlayerMap<PlayerConfig>) -> Self {
         BotRunner {
             processes: players.iter().map(|(&id, config)| {
-                let process = process::Command::new(&config.command)
-                    .args(&config.args)
-                    .arg(format!("{}", config.name.as_str()))
-                    .stdin(process::Stdio::piped())
-                    .stdout(process::Stdio::piped())
-                    .spawn_async(handle)
+                let mut cmd = Command::new(&config.command);
+                cmd.args(&config.args);
+                cmd.arg(format!("{}", config.name.as_str()));
+
+                let handle = BotHandle::spawn(cmd, handle)
                     .expect(&format!(
                         "\n[DRIVER] Failed to execute process: {} {:?}\n",
                         config.command,
                         config.args
                     ));
-                return (id, process);
+                unimplemented!()
             }).collect()
         }
     }
@@ -67,6 +66,8 @@ impl Read for BotHandle {
     }
 }
 
+impl AsyncRead for BotHandle {}
+
 impl Write for BotHandle {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
         self.stdin.write(buf)
@@ -77,8 +78,22 @@ impl Write for BotHandle {
     }
 }
 
+impl AsyncWrite for BotHandle {
+    fn shutdown(&mut self) -> Poll<(), Error> {
+        self.stdin.shutdown()
+    }
+}
+
 impl BotHandle {
-    fn new(mut process: Child) -> BotHandle {
+    pub fn spawn(mut command: Command, handle: &Handle) -> Result<BotHandle> {
+        command
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn_async(handle)
+            .map(|process| BotHandle::new(process))
+    }
+
+    pub fn new(mut process: Child) -> BotHandle {
         BotHandle {
             stdin: process.stdin().take().unwrap(),
             stdout: process.stdout().take().unwrap(),
