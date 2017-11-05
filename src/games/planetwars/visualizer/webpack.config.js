ac7c5136dbee8dc06703a77e0a54f2985a4c2145
Wout Schellaert <wout.schellaert@gmail.com>
2017-11-05 18:57:12 +0100

    Make less work

--- src/games/planetwars/visualizer/webpack.config.js
+++ src/games/planetwars/visualizer/webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractLess = new ExtractTextPlugin({
+    filename: "[name].css"
+})
+
 module.exports = {
-  entry: "./src/app.js",
+  entry: ["./src/app.js", "./src/assets/style/main.less"],
   output: {
     path: __dirname + '/app/',
     filename: "bundle.js",
@@ -14,8 +20,21 @@ module.exports = {
     ]
   },
   module: {
-    loaders: [
-      { test: /\.css$/, loader: "style!css" }
-    ]
-  }
+    rules: [{
+      test: /\.less$/,
+      use: extractLess.extract({
+        use: [{
+          loader: "css-loader"
+        }, {
+          loader: "less-loader"
+        }]
+      }), 
+    }, {
+      test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
+      loader: 'file-loader',
+    }]
+  },
+  plugins: [
+    extractLess
+  ]
 };
