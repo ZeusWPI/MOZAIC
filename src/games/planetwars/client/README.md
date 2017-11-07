badbae6affea663221e5127e32a3348a03530550
Wout Schellaert <wout.schellaert@gmail.com>
2017-11-07 17:03:23 +0100

    Hello world electron

---
+++ src/games/planetwars/client/README.md
@@ -0,0 +1,9 @@
+# Readme
+
+## Run
+
+1. `npm install -g typescript`
+1. `npm install -g gulp-cli`
+1. `npm install`
+1. `gulp` or `gulp watch`
+1. `npm run start`
---
+++ src/games/planetwars/client/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var del = require('del');
+var merge = require('merge-stream');
+var sourcemaps = require('gulp-sourcemaps');
+
+var dest = 'dist'
+
+gulp.task('clean:output', function () {
+  return del([
+    'dist/'
+  ]);
+});
+
+gulp.task('watch', ['compile'], function(){
+  gulp.watch(['**/*.ts', '!**/dist'], ['default'], { ignoreInitial: false });
+});
+
+
+gulp.task('compile', ['clean:output'], function(){
+    var tsProject = ts.createProject('tsconfig.json');
+    var result = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    var js = result.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dest));
+
+    var html = gulp.src('**/*.html')
+          .pipe(gulp.dest(dest));
+
+    var dts = result.dts.pipe(gulp.dest(dest));
+
+    return merge(js, dts, html);
+});
+
+gulp.task('default', ['compile']);
