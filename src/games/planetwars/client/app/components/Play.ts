1ff29e236e3107c42ce7e207e556b34a5fb913b9
Robbe Van Herck <robbevanherck1@gmail.com>
2017-12-17 17:31:20 +0100

    Remade the Play page using the new bot-system

--- src/games/planetwars/client/app/components/Play.ts
+++ src/games/planetwars/client/app/components/Play.ts
@@ -3,17 +3,12 @@ import * as path from 'path';
 
 import { h, div } from 'react-hyperscript-helpers';
 
-import { Visualizer } from './visualizer/index';
-import { GameSetup } from './gameSetup/GameSetup';
-import { GamePlayer } from './gamePlayer/GamePlayer';
-
+import { Setup } from './play/Setup';
 
 let styles = require('./Play.scss');
 
 interface State {
-  configPath?: path.ParsedPath,
-  gamelog: path.ParsedPath,
-  configMode: boolean
+
 }
 
 
@@ -23,30 +18,11 @@ export default class Play extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      configMode: true,
-      gamelog: path.parse("./log.json")
+
     };
   }
 
   render() {
-    if(this.state.configMode) {
-      return div(`.${styles.playPage}`, [
-        div(`.${styles.gameSetup}`, [
-          h(GameSetup, {
-            onReady: (p: path.ParsedPath) => this.setState({configPath: p})
-          })
-        ]),
-        div(`.${styles.gamePlayer}`, [
-          h(GamePlayer, {
-            configPath: this.state.configPath,
-            callback: () => {
-              this.setState({ configMode: false });
-            }
-          })
-        ])
-      ])
-    } else {
-      return div(`.${styles.visualizer}`, [h(Visualizer, <any>{ gamelog: this.state.gamelog })]);
-    }
+    return h(Setup)
   }
 }
