31e53a10a4bf14f3300c4ccc49ef241afdb8e177
Ilion Beyst <ilion.beyst@gmail.com>
2017-12-19 18:25:24 +0100

    change some names

--- src/gameserver/src/planetwars/step_lock.rs
+++ src/gameserver/src/planetwars/step_lock.rs
@@ -2,28 +2,28 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub struct StepLock {
-    awaiting_input_map: HashMap<usize, String>,
+    client_messages: HashMap<usize, String>,
     awaiting_clients: HashSet<usize>,
 }
 
 impl StepLock {
     pub fn new() -> StepLock {
         StepLock {
-            awaiting_input_map: HashMap::new(),
+            client_messages: HashMap::new(),
             awaiting_clients: HashSet::new(),
         }
     }
 
     pub fn wait_for(&mut self, client_id: usize){
-        awaiting_clients.insert(client_id);
+        self.awaiting_clients.insert(client_id);
     }
 
-    pub fn has_all_commands(&self) -> bool {
+    pub fn is_ready(&self) -> bool {
         self.awaiting_clients.is_empty()
     }
 
     pub fn attach_command(&mut self, client_id: usize, msg: String){
-        self.awaiting_input_map.insert(client_id, msg);
+        self.client_messages.insert(client_id, msg);
         self.awaiting_clients.remove(&client_id);
     }
 }
