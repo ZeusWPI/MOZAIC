a817b09202bae626511c6e7885e7ef4983a79c4d
Wout Schellaert <wout.schellaert@gmail.com>
2018-02-13 01:09:53 +0100

    Begin signup page

--- planetwars/web/frontend/components/Signup.ts
+++ planetwars/web/frontend/components/Signup.ts
@@ -1,15 +1,102 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { h, div, p } from 'react-hyperscript-helpers';
+import { h, div, p, form, input, a, label } from 'react-hyperscript-helpers';
 import NavBar from './Navbar';
 import Footer from './Footer';
+import { Hero } from 'bloomer';
+import { Container } from 'bloomer/lib/layout/Container';
+import { HeroBody } from 'bloomer/lib/layout/Hero/HeroBody';
+import { error } from 'util';
 
 export default class Signup extends React.Component<{}, {}> {
   render() {
     return [
       h(NavBar),
-      div([p('Info')]),
+      h(Hero, '.push', {isColor: 'primary'}, [
+          h(HeroBody, '#signup-page', [
+            h(SignupBox)
+          ])
+      ]),
       h(Footer),
     ]
+  }
+}
+
+interface ISignupBoxState {
+  email: string,
+  success: boolean,
+  errors: {message: string}[]
+}
+class SignupBox extends React.Component<{}, ISignupBoxState> {
+  constructor(props: {}){
+    super(props);
+    this.state = {email: '', success: false, errors: []};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({email: event.target.value});
+  }
+
+  subscribe(): Promise<Response> {
+    return fetch('/subscribe', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.email
+      })
+    })
+  }
+
+  handleSubmit(event: any) {
+    event.preventDefault();
+    this.subscribe().then((resp: Response) => {
+      if(resp.ok) {
+        this.setState({success: true});
+        return Promise.resolve();
+      } 
+
+      if(resp.status === 400) {
+        resp.text().then((text) => {
+          this.setState({errors: JSON.parse(text)})
+        });
+        return Promise.resolve();
+      }
+
+      return Promise.reject(resp);
+    }).catch((error) => {console.log(error); alert("Something went wrong, see console"); });
+  }
+  
+  render() {
+    if(this.state.success) {
+      return div('#signup-box.container', [
+        p('Well received! See you later...')
+      ])
+    }
+
+    return div('#signup-box.container', [
+      form({action: '/subscribe', method: 'POST', onSubmit: this.handleSubmit}, [
+        label('.label', {htmlFor: 'email'}, ['Enter email and keep up to date!']),
+        div('.field.has-addons', [
+          div('.control', [
+            input('.input', {
+              id: 'email',
+              type: 'text',
+              value: this.state.email,
+              onChange: this.handleChange,
+              placeholder: 'eg. bottle@bats.com'
+            })
+          ]),
+          div('.control', [
+            input('.button.submit', {type: 'submit', value: 'Subscribe'})
+          ]),
+          p(this.state.errors.map(v => v.message))
+        ])
+      ])
+    ])
   }
 }
--- planetwars/web/src/main.rs
+++ planetwars/web/src/main.rs
@@ -1,13 +1,18 @@
 
 #![feature(plugin)]
+#![feature(custom_derive)]
 #![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
 use std::io;
 use std::path::{Path, PathBuf};
-
+use rocket_contrib::Json;
 use rocket::response::NamedFile;
+use rocket::response::status;
 
-extern crate rocket;
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -19,6 +24,18 @@ fn assets(asset: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("dist/").join(asset)).ok()
 }
 
+#[derive(Deserialize)]
+struct SubscribeForm {
+    email: String
+}
+
+#[post("/subscribe", format = "application/json", data = "<form>")]
+fn subscribe(form: Json<SubscribeForm>) -> status::NoContent<> {
+    let form: SubscribeForm = form.into_inner();
+    println!("{}", form.email);
+    status::NoContent
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, assets]).launch();
+    rocket::ignite().mount("/", routes![index, assets, subscribe]).launch();
 }
