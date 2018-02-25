27dec924800dc7740b1c27b63dc170bb678be1bf
Ilion Beyst <ilion.beyst@gmail.com>
2018-02-25 21:45:23 +0100

    start writing connection class

---
+++ src/client/src/connection.ts
@@ -0,0 +1,24 @@
+import * as protocol from './proto';
+import * as net from 'net';
+
+const proto  = protocol.mozaic.client_server;
+
+export class Connection {
+    private socket: net.Socket;
+    private token: Buffer;
+
+    public constructor(host: string, port: number, token: Buffer) {
+        this.token = token;
+
+        
+        this.socket = net.connect(port, host, () => {
+            this.send_message(proto.Connect, { token: token });
+        });
+    }
+
+    private send_message(type, params) {
+        let msg = type.create(params);
+        let bytes = type.encodeDelimited(msg).finish();
+        this.socket.write(bytes);
+    }
+}
--- src/client/src/index.ts
+++ src/client/src/index.ts
@@ -1,27 +1,3 @@
-import * as protocol from './proto';
-import * as net from 'net';
+import { Connection } from './connection';
 
-const proto  = protocol.mozaic.client_server;
-
-function connect() {
-    let client = new net.Socket();
-
-    client.connect(9142, '127.0.0.1', function() {
-        console.log('connected');
-        let msg = proto.Connect.create({
-            "token": Buffer.from("1"),
-        });
-        client.write(proto.Connect.encodeDelimited(msg).finish());
-    });
-
-    client.on('data', function(data) {
-	    console.log('Received: ' + data);
-	    client.destroy()
-    });
-
-    client.on('close', function() {
-        console.log('Connection closed');
-    });
-}
-
-connect();
+new Connection("127.0.0.1", 9142, Buffer.from("0102030405", "hex"));
