b09245b3c3b8925dc59bb2e4b93ab50fad05fb17
Ilion Beyst <ilion.beyst@gmail.com>
2018-03-03 14:29:24 +0100

    use ProtobufTransport in ClientController

--- src/gameserver/src/client_controller/client_controller.rs
+++ src/gameserver/src/client_controller/client_controller.rs
@@ -1,17 +1,14 @@
-use futures::{Future, Poll, Async};
+use futures::{Future, Poll, Async, Stream};
 use futures::sync::mpsc::{unbounded, UnboundedSender, UnboundedReceiver};
-use futures::stream::{Stream, SplitStream, SplitSink};
-use tokio_io::AsyncRead;
 use tokio::net::TcpStream;
-use tokio_io::codec::Framed;
-use tokio_io::codec::{Encoder, Decoder};
-use bytes::{BytesMut, BufMut};
+use bytes::BytesMut;
 use std::io;
 use std::str;
 use slog;
 
 use super::client_connection::ClientConnection;
-use bot_runner::BotHandle;
+use protobuf_codec::ProtobufTransport;
+
 
 
 error_chain! {
@@ -30,16 +27,17 @@ pub struct ClientMessage {
 }
 
 pub enum Message {
-    Data(String),
+    Data(Vec<u8>),
     Disconnected,
 }
 
 pub enum Command {
-    Send(String),
+    Send(Vec<u8>),
     Connect(TcpStream),
 }
 
-// TODO: the client controller should also be handed a log handle
+// TODO: maybe use a type parameter instead of hardcoding
+type Transport = ProtobufTransport<TcpStream>;
 
 
 pub struct ClientController {
@@ -96,18 +94,7 @@ impl ClientController {
         self.game_handle.unbounded_send(msg).expect("game handle broke");
     }
 
-    /// Pull messages from the client, and handle them.
-    fn handle_client_msgs(&mut self) -> Result<()> {
-        while let Some(line) = try!(self.connection.poll()) {
-            
-        }
-        return Ok(());
-    }
 
-    /// The unit error type of ctrl_chan.poll() means that it won't error. Since
-    /// we can't cast "won't error" to our custom error type, we cannot use the
-    /// try_ready! macro for polling the ctrl_chan. This method provides an
-    /// adapter to Poll with our error type.
     fn poll_ctrl_chan(&mut self) -> Async<Command> {
         // we hold a handle to this channel, so it can never close.
         // this means errors can not happen.
@@ -119,36 +106,26 @@ impl ClientController {
     fn handle_commands(&mut self) {
         while let Async::Ready(command) = self.poll_ctrl_chan() {
             match command {
-                Command::Send(message) => self.connection.queue_send(message),
-                Command::Connect(sock) => unimplemented!(),
+                Command::Send(message) => {
+                    let bytes = BytesMut::from(message);
+                    self.connection.queue_send(bytes);
+                },
+                Command::Connect(_sock) => unimplemented!(),
             }
         }
     }
 
     fn poll_client_connection(&mut self) -> Result<()> {
         try!(self.connection.flush());
-        while let Some(msg) = try!(self.connection.poll()) {
-            self.handle_client_message(msg);
+        while let Some(bytes) = try!(self.connection.poll()) {
+            self.handle_client_message(bytes.freeze().to_vec());
         }
         return Ok(());
     }
 
-    fn handle_client_message(&mut self, msg: String) {
-        let data = Message::Data(msg);
+    fn handle_client_message(&mut self, bytes: Vec<u8>) {
+        let data = Message::Data(bytes);
         self.send_message(data);
-    }
-
-    /// Step the future, allowing errors to be thrown.
-    /// These errors then get handled in the actual poll implementation.
-    fn try_poll(&mut self) -> Poll<(), Error> {
-        let res = self.poll_client_connection();
-        if let Err(err) = res {
-            self.connection.drop_transport();
-            // TODO: log
-        }
-        self.handle_commands();
-
-        Ok(Async::NotReady)
     }
 }
 
@@ -166,50 +143,5 @@ impl Future for ClientController {
         self.handle_commands();
         // TODO: proper exit
         Ok(Async::NotReady)
-    }
-}
-
-
-
-// This is rather temporary.
-type Transport = Framed<BotHandle, LineCodec>;
-
-pub struct LineCodec;
-
-impl Encoder for LineCodec {
-    type Item = String;
-    type Error = io::Error;
-
-    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
-        buf.reserve(msg.len() + 1);
-        buf.extend(msg.as_bytes());
-        buf.put_u8(b'\n');
-        Ok(())
-    }
-}
-
-impl Decoder for LineCodec {
-    type Item = String;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
-        // Check to see if the frame contains a new line
-        if let Some(n) = buf.as_ref().iter().position(|b| *b == b'\n') {
-            // remove line from buffer
-            let line = buf.split_to(n);
-
-            // remove newline
-            buf.split_to(1);
-
-            // Try to decode the line as UTF-8
-            return match str::from_utf8(&line.as_ref()) {
-                Ok(s) => Ok(Some(s.to_string())),
-                Err(_) => Err(io::Error::new(
-                    io::ErrorKind::Other, "invalid string")
-                ),
-            }
-        }
-
-        Ok(None)
     }
 }
