3816b36e79f01fcabe14b7e6f51b0e297c98eb79
Ilion Beyst <ilion.beyst@gmail.com>
2018-03-04 01:39:04 +0100

    fix packet type

--- src/gameserver/src/router/types.rs
+++ src/gameserver/src/router/types.rs
@@ -3,7 +3,7 @@ use futures::sync::mpsc::{UnboundedSender, SendError};
 pub struct Packet {
     pub connection_id: u64,
     pub transport_id: u64,
-    pub data: Vec<u64>,
+    pub data: Vec<u8>,
 }
 
 
