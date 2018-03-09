ac58832b6e0e587de6a4d4ac2eb64a9be145b26a
Ilion Beyst <ilion.beyst@gmail.com>
2018-03-09 20:05:21 +0100

    set up main

--- src/gameserver/src/main.rs
+++ src/gameserver/src/main.rs
@@ -1,10 +1,9 @@
 mod bot_runner;
 mod client_controller;
-mod planetwars;
-mod transports;
-
 mod protobuf_codec;
+mod planetwars;
 mod router;
+
 pub mod protocol {
     include!(concat!(env!("OUT_DIR"), "/mozaic.protocol.rs"));
 }
@@ -55,16 +54,75 @@ use tokio_io::{AsyncRead, AsyncWrite};
 use prost::Message;
 use futures::Future;
 
-use bot_runner::*;
 use protobuf_codec::ProtobufTransport;
 
 use client_controller::ClientController;
 use planetwars::{Controller, Client};
+use router::Router;
 
 // Load the config and start the game.
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Expected 1 argument (config file). {} given.", args.len() - 1);
+        std::process::exit(1)
+    }
+
+    let match_description: MatchDescription = match parse_config(Path::new(&args[1])) {
+        Ok(config) => config,
+        Err(e) => {
+            println!("{}", e);
+            std::process::exit(1)
+        }
+    };
+
+    let mut reactor = Core::new().unwrap();
+
+    let log_file = File::create("log.json").unwrap();
+
+    let logger = slog::Logger::root( 
+        Mutex::new(slog_json::Json::default(log_file)).map(slog::Fuse),
+        o!()
+    );
+
+
+    let (router_handle, router_chan) = mpsc::unbounded();
+    let (controller_handle, controller_chan) = mpsc::unbounded();
+
+    let handles = match_description.players.iter().enumerate().map(|(num, desc)| {
+        let mut controller = ClientController::new(
+            num,
+            desc.token.clone(),
+            router_handle.clone(),
+            controller_handle.clone(),
+            &logger);
+        let ctrl_handle = controller.handle();
+        controller.register();
+        reactor.handle().spawn(controller);
+
+        Client {
+            id: num,
+            player_name: desc.name.clone(),
+            // TODO
+            handle: ctrl_handle,
+        }
+    }).collect();
+
+
+    let controller = Controller::new(
+        handles,
+        controller_chan,
+        match_description.game_config,
+        logger,
+    );
+    reactor.handle().spawn(controller);
+
+    let router = Router::new(router_chan);
+    reactor.handle().spawn(router);
+
     let addr = "127.0.0.1:9142".parse().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
+
     let server = listener.incoming().for_each(|socket| {
         println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
         let transport = ProtobufTransport::new(socket);
@@ -80,65 +138,7 @@ fn main() {
     });
 
 
-    let mut reactor = Core::new().unwrap();
     reactor.run(server).unwrap();
-
-    
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        println!("Expected 1 argument (config file). {} given.", args.len() - 1);
-        std::process::exit(1)
-    }
-
-    let match_description: MatchDescription = match parse_config(Path::new(&args[1])) {
-        Ok(config) => config,
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(1)
-        }
-    };
-
-
-
-    let log_file = File::create("log.json").unwrap();
-
-    let logger = slog::Logger::root( 
-        Mutex::new(slog_json::Json::default(log_file)).map(slog::Fuse),
-        o!()
-    );
-
-    let mut bots = spawn_bots(&reactor.handle(), &match_description.players);
-
-    let (handle, chan) = mpsc::unbounded();
-
-
-    let handles = match_description.players.iter().enumerate().map(|(num, desc)| {
-        let bot_handle = bots.remove(&desc.name).unwrap();
-        let controller = ClientController::new(
-            num,
-            unimplemented!(),
-            handle.clone(),
-            &logger);
-        let ctrl_handle = controller.handle();
-        reactor.handle().spawn(controller);
-
-        Client {
-            id: num,
-            player_name: desc.name.clone(),
-            // TODO
-            handle: unimplemented!(),
-        }
-    }).collect();
-
-
-    let controller = Controller::new(
-        handles,
-        chan,
-        match_description.game_config,
-        logger,
-    );
-
-    reactor.run(controller).unwrap();
 }
 
 #[derive(Serialize, Deserialize)]
@@ -147,6 +147,13 @@ pub struct MatchDescription {
     pub game_config: planetwars::Config,
     pub log_file: Option<String>
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct PlayerConfig {
+    pub name: String,
+    pub token: Vec<u8>,
+}
+
 
 // Parse a config passed to the program as an command-line argument.
 // Return the parsed config.
