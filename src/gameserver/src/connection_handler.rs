779d9dd393d597bf5e7d61b8404dd8c391b86245
Ilion Beyst <ilion.beyst@gmail.com>
2018-03-11 19:13:10 +0100

    some hacks, but succesfully deliver connection to client controller

--- src/gameserver/src/connection_handler.rs
+++ src/gameserver/src/connection_handler.rs
@@ -1,23 +1,22 @@
 use tokio::net::TcpStream;
-use futures::{Future, Poll, Async, Stream};
+use futures::{Future, Poll, Async, Stream, Sink, StartSend, AsyncSink};
 use futures::sync::mpsc::{UnboundedSender, UnboundedReceiver};
 use futures::sync::oneshot;
 use prost::Message;
 use std::io;
+use bytes::BytesMut;
 
 use protobuf_codec::ProtobufTransport;
 use client_controller::Command as ClientControllerCommand;
 use protocol;
 use router;
 
-
-
 struct Waiting;
 
 impl Waiting {
     fn poll(&mut self, data: &mut HandlerData) -> Poll<HandlerState, io::Error>
     {
-        let bytes  = match try_ready!(data.transport.poll()) {
+        let bytes  = match try_ready!(data.conn_mut().poll()) {
             None => bail!(io::ErrorKind::ConnectionAborted),
             Some(bytes) => bytes,
         };
@@ -56,7 +55,64 @@ impl Lookup {
             None => panic!("invalid token"),
             Some(handle) => handle, 
         };
-        println!("got handle");
+        // TODO: this is not nice
+        let response = protocol::ConnectResponse {
+            response: Some(
+                protocol::connect_response::Response::Connected(
+                    protocol::Connected::default()
+                )
+            )
+        };
+        let mut buf = BytesMut::new();
+        try!(response.encode(&mut buf));
+        let accepting = Accepting {
+            send: SendRef::new(buf),
+            handle,
+        };
+        return Ok(Async::Ready(HandlerState::Accepting(accepting)));
+    }
+}
+
+struct SendRef<T> {
+    item: Option<T>,
+}
+
+impl<T> SendRef<T> {
+    pub fn new(item: T) -> Self {
+        SendRef {
+            item: Some(item),
+        }
+    }
+
+    pub fn poll_send<S>(&mut self, sink: &mut S) -> Poll<(), S::SinkError>
+        where S: Sink<SinkItem = T>
+    {
+        if let Some(item) = self.item.take() {
+            match try!(sink.start_send(item)) {
+                AsyncSink::NotReady(item) => {
+                    self.item = Some(item);
+                    return Ok(Async::NotReady);
+                },
+                AsyncSink::Ready => (),
+            }
+        }
+
+        return sink.poll_complete();
+    }
+}
+
+struct Accepting {
+    send: SendRef<BytesMut>,
+    handle: UnboundedSender<ClientControllerCommand>,
+}
+
+impl Accepting {
+    fn poll(&mut self, data: &mut HandlerData) -> Poll<HandlerState, io::Error>
+    {
+        try_ready!(self.send.poll_send(data.conn_mut()));
+        self.handle.unbounded_send(ClientControllerCommand::Connect(
+            data.transport.take().unwrap()
+        )).unwrap();
         return Ok(Async::Ready(HandlerState::Done));
     }
 }
@@ -64,6 +120,7 @@ impl Lookup {
 enum HandlerState {
     Waiting(Waiting),
     Lookup(Lookup),
+    Accepting(Accepting),
     Done,
 }
 
@@ -73,14 +130,25 @@ impl HandlerState {
         match *self {
             HandlerState::Waiting(ref mut waiting) => waiting.poll(data),
             HandlerState::Lookup(ref mut lookup) => lookup.poll(data),
+            HandlerState::Accepting(ref mut accepting) => accepting.poll(data),
             HandlerState::Done => panic!("polling Done"),
         }
     }
 }
 
 struct HandlerData {
-    transport: ProtobufTransport<TcpStream>,
+    transport: Option<ProtobufTransport<TcpStream>>,
     router_handle: UnboundedSender<router::TableCommand>,
+}
+
+impl HandlerData {
+    // TODO: this might not be the ideal solution ...
+    fn conn_mut<'a>(&'a mut self) -> &'a mut ProtobufTransport<TcpStream> {
+        match self.transport.as_mut() {
+            Some(conn) => conn,
+            None => panic!("Connection moved"),
+        }
+    }
 }
 
 pub struct ConnectionHandler {
@@ -96,7 +164,7 @@ impl ConnectionHandler {
         ConnectionHandler {
             state: HandlerState::Waiting(Waiting),
             data: HandlerData {
-                transport,
+                transport: Some(transport),
                 router_handle,
             }
         }
