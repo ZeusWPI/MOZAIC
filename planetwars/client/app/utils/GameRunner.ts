b6c14a24193e718f789bd28c0387552ffd902ce9
Robbe Van Herck <robbevanherck1@gmail.com>
2018-03-19 14:41:12 +0100

    Basic end-of-game notifications

--- planetwars/client/app/utils/GameRunner.ts
+++ planetwars/client/app/utils/GameRunner.ts
@@ -1,34 +1,42 @@
 import { store } from "../index";
 import { IMatchConfig } from "./ConfigModels";
-import { matchStarted, matchFinished, matchCrashed } from '../actions/actions';
-import { Config } from "./Config"
-import { v4 as uuidv4 } from "uuid"
-import * as fs from "fs"
+import { matchStarted, matchFinished, matchCrashed, addNotification } from '../actions/actions';
+import { Config } from "./Config";
+import { v4 as uuidv4 } from "uuid";
+import * as fs from "fs";
 
 const execFile = require('child_process').execFile;
 
 export default class GameRunner {
-  conf: IMatchConfig;
+  public conf: IMatchConfig;
   constructor(conf: IMatchConfig) {
     this.conf = conf;
     this.runBotRunner();
   }
 
-  runBotRunner() {
-    store.dispatch(matchStarted())
-    let configFile = this.createConfig(JSON.stringify(this.conf))
+  public runBotRunner() {
+    store.dispatch(matchStarted());
+    const configFile = this.createConfig(JSON.stringify(this.conf));
     const child = execFile(Config.matchRunner, [configFile], ((error: any, stdout: any, stderr: any) => {
       if (error) {
-        store.dispatch(matchCrashed(error))
+        store.dispatch(matchCrashed(error));
+        store.dispatch(addNotification({
+          title: "Match has crashed",
+          body: "" + error,
+        }));
       } else {
-        store.dispatch(matchFinished())
+        store.dispatch(matchFinished());
+        store.dispatch(addNotification({
+          title: "Match has finished",
+          body: "Click here for more info",
+        }));
       }
     }));
   }
 
-  createConfig(json: string) {
-    let path = Config.configPath(uuidv4())
-    fs.writeFileSync(path, json)
-    return path
+  public createConfig(json: string) {
+    const path = Config.configPath(uuidv4());
+    fs.writeFileSync(path, json);
+    return path;
   }
 }
