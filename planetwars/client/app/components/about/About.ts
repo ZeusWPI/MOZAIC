d8ad7f484f5a295fcfb8f11d0135a85c004d0d0c
Arne <arnebertrand@gmail.com>
2018-03-22 15:47:27 +0100

    Correction of some details

--- planetwars/client/app/components/about/About.ts
+++ planetwars/client/app/components/about/About.ts
@@ -19,6 +19,9 @@ export default class About extends React.Component<IProps, IState> {
       h2(`.${styles.subTitle}`, 'Getting Started'),
 
       p(['In order to start a game, you will need at least 2 bots. ' +
+        'Feel free to use the example bots provided. ' +
+        'Unfortunately, selecting a single bot multiple times is currently not supported. ' +
+        'To play a bot against itself, please add the bot to the client twice (see below). ' +
         'A “bot” is what we call the script you will write which will play the game. ' +
         'For more details on the the rules of PlanetWars and the scripting API, ' +
         'please refer to ',
@@ -34,15 +37,16 @@ export default class About extends React.Component<IProps, IState> {
         'and which will be shown on the victory screen, so choose well!'),
 
       p('Command: This is the command you would normally enter in your terminal to run your script. ' +
-        'Make sure all required arguments (such as the filename) are provided here as well.' +
-        'E.g: python3 “~/bots/my_awesome_bot.py”'),
+        'Make sure all required arguments (such as the filename) are provided here as well. ' +
+        'Please use absolute paths as shell expansion is currently not supported. ' +
+        'E.g: python3 “/home/YOUR_USERNAME/bots/my_awesome_bot.py”'),
 
       h2(`.${styles.subTitle}`, 'Playing A Game'),
 
       p(['With the bots configured as above, head on over to the ',
         h(Link, {to: '/Play'}, ['Play']),
         ' page. Here you will see all the bots you have registered. ' +
-        'Select all the bots you would like to enter into battle. To deselect a bot, just click it again.' +
+        'Select all the bots you would like to enter into battle. To deselect a bot, just click it again. ' +
         'Next, choose the map you would like to play on, or import your own. ' +
         'Set a turn limit, and finally, hit the Play button.']),
 
@@ -51,9 +55,10 @@ export default class About extends React.Component<IProps, IState> {
       p(['Once the game has finished playing, you can then load up the gamelog in the visualizer ' +
       'in order to fine-tune your strategy for the next match. ' +
       'To do this, go to the ',
-        h(Link, {to: '/Visualizer'}, ['Visualizer']),
-        ' page and press the “load game” button. ' +
-        'Once the game is loaded, you can control the playback with the buttons at the bottom of the page.']),
+        h(Link, {to: '/history'}, ['Matches']),
+        ' page. Here you will see an overview of your completed matches. You can then select a match to load it into the ',
+        h(Link, {to: '/Visualizer'}, ['visualizer']),
+        '. Once the game is loaded, you can control the playback with the buttons at the bottom of the page.']),
 
     ]);
 
