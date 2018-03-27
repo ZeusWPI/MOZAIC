6cda4ead3ce7b35acae06127b773d82d4682021f
Ilion Beyst <ilion.beyst@gmail.com>
2018-03-27 19:52:24 +0200

    fix build

--- src/gameserver/build.rs
+++ src/gameserver/build.rs
@@ -1,6 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["../client_server.proto"],
+    prost_build::compile_protos(&["../proto/core.proto"],
                                 &[".."]).unwrap();
 }
--- src/gameserver/src/client_controller/mod.rs
+++ src/gameserver/src/client_controller/mod.rs
@@ -1,5 +1,4 @@
 mod client_controller;
 mod client_connection;
-mod receiver;
 
 pub use self::client_controller::*;
