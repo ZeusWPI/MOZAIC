81a74e02bd7b86ecfe8b48774dea4ae21c6851bc
Robbe Van Herck <robbevanherck1@gmail.com>
2018-03-27 09:52:37 +0200

    woops, it broke

--- planetwars/client/app/utils/Database.ts
+++ planetwars/client/app/utils/Database.ts
@@ -64,7 +64,7 @@ export function bindToStore(store: any): Promise<void> {
     }).write())
     .then((db) => {
       if (db.version as string !== 'v3') {
-        return db.add(migrateOld(db));
+        return migrateOld(db);
       }
       return db;
     })
