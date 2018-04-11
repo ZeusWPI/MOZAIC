43a00d9882eefb75f2a134c7184d6fb20ce68bc3
Robbe Van Herck <robbevanherck1@gmail.com>
2018-04-11 18:59:37 +0200

    Using Stateless Functional Component

--- planetwars/client/app/components/info/Info.tsx
+++ planetwars/client/app/components/info/Info.tsx
@@ -23,16 +23,9 @@ export default class Info extends React.Component<{}, { mapToggled: boolean }> {
         README
       </a>);
     const toggle = (evt: any) => this.setState({ mapToggled: !this.state.mapToggled });
-    let version = __BRANCHNAME__ + "@" + __COMMIT_HASH__;
-    if (__TAG__ !== "") {
-      version = __TAG__;
-    }
     return (
       <div className={styles.infoPage}>
-
-        <div className={styles.version}>
-          Build: {version}
-        </div>
+        <Version />
         <MapPanel toggled={this.state.mapToggled} toggle={toggle} />
         <div className='container'>
           <div>
@@ -99,6 +92,18 @@ export default class Info extends React.Component<{}, { mapToggled: boolean }> {
     );
   }
 }
+
+export const Version: React.SFC<{}> = (props) => {
+  let version = __BRANCHNAME__ + "@" + __COMMIT_HASH__;
+  if (__TAG__ !== "") {
+    version = __TAG__;
+  }
+  return (
+    <div className={styles.version}>
+      Build: {version}
+    </div>
+  );
+};
 
 export class MapPanel extends React.Component<{ toggled: boolean, toggle: (evt: any) => void }, {}> {
   public render() {
