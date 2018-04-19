bc8df32d1a825a44acf703d42ff0af4db0897a82
Robbe Van Herck <robbevanherck1@gmail.com>
2018-04-19 15:14:50 +0200

    Changed from lambda's in onClick, onChange, ...

--- planetwars/client/app/components/host/Host.tsx
+++ planetwars/client/app/components/host/Host.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
-import { BotConfig, IBotList, IBotData, BotID, BotSlot, Token, BotSlotList, MatchConfig } from '../../utils/ConfigModels';
+import { BotConfig, IBotList, IBotData, BotID, BotSlot, Token, BotSlotList,
+  MatchConfig } from '../../utils/ConfigModels';
 import { IMapList } from "../../utils/GameModels";
 import { MatchParams } from '../../actions/actions';
 
@@ -30,8 +31,8 @@ export class Host extends React.Component<HostProps, HostState> {
     return (
       <div>
         <div>
-          <button onClick={() => this.addInternal()}>+ Add one of your own bots</button>
-          <button onClick={() => this.addExternal()}>+ Add an external bot</button>
+          <button onClick={this.addInternal}>+ Add one of your own bots</button>
+          <button onClick={this.addExternal}>+ Add an external bot</button>
         </div>
         <div>
           <BotSlots
@@ -41,12 +42,12 @@ export class Host extends React.Component<HostProps, HostState> {
           />
         </div>
         <MapSelector maps={this.props.maps} selectMap={this.props.selectMap} selectedMap={this.props.selectedMap}/>
-        <button onClick={() => this.startServer()}>Play!</button>
+        <button onClick={this.startServer}>Play!</button>
       </div>
     );
   }
 
-  private startServer() {
+  private startServer = () => {
     const config: MatchParams = {
       bots: this.props.selectedBots,
       map: this.props.selectedMap,
@@ -56,11 +57,11 @@ export class Host extends React.Component<HostProps, HostState> {
     this.props.runMatch(config);
   }
 
-  private addInternal() {
+  private addInternal = () => {
     this.props.selectBotInternal("My Bot", "");
   }
 
-  private addExternal() {
+  private addExternal = () => {
     this.props.selectBotExternal("My Enemy's Bot");
   }
 }
@@ -106,7 +107,7 @@ export class Slot extends React.Component<SlotProps> {
       });
       extra = (
         <div>
-            <select value={this.props.bot.id} onChange={(evt) => {this.changeBotID(evt.target.value)}}>
+            <select value={this.props.bot.id} onChange={this.changeBotID}>
               <option value="">Select Bot</option>
               {options}
           </select>
@@ -121,18 +122,21 @@ export class Slot extends React.Component<SlotProps> {
         Name: <input
           type="text"
           defaultValue={this.props.bot.name}
-          onBlur={(evt) => this.changeBotName(evt.target.value)}/>
+          onBlur={this.changeBotName}
+        />
         {extra}
       </li>);
   }
 
-  private changeBotID(id: BotID) {
+  private changeBotID = (evt: React.ChangeEvent<HTMLSelectElement>) => {
+    const id: BotID = evt.target.value;
     const newBot = this.props.bot;
     newBot.id = id;
     this.props.changeLocalBot(this.props.token, newBot);
   }
 
-  private changeBotName(name: string) {
+  private changeBotName(evt: any) {
+    const name: string = evt.target.value;
     const newBot = this.props.bot;
     newBot.name = name;
     this.props.changeLocalBot(this.props.token, newBot);
@@ -149,9 +153,10 @@ export const MapSelector: React.SFC<MapSelectorProps> = (props) => {
   const mapElements = Object.keys(props.maps).map(
     (key, idx) => <option value={key} key={idx}>{props.maps[key].name}</option>,
   );
+  const handleChange = (evt: any) => props.selectMap(evt.target.value);
   mapElements.unshift(<option value="">Select Map</option>);
   return (
-    <select value={props.selectedMap} onChange={(evt) => props.selectMap(evt.target.value)}>
+    <select value={props.selectedMap} onChange={handleChange}>
       {mapElements}
     </select>
   );
