b5d5856c316455e014db6fc64ff793adb68f1f1e
Ilion Beyst <ilion.beyst@gmail.com>
2018-04-21 17:03:23 +0200

    collect garbage

--- gameserver/src/games/mod.rs
+++
@@ -1,9 +0,0 @@
-// TODO: deprecate this module
-
-// mod higher_lower;
-// pub use self::higher_lower::*;
-
-// mod shit_planets;
-// pub use self::shit_planets::ShitPlanets;
-
-// pub mod planetwars;
