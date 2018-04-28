a1215522c099164ef02abb6b6d118d9cf58c27bc
Ilion Beyst <ilion.beyst@gmail.com>
2018-04-28 15:54:16 +0200

    creat addressform

---
+++ planetwars/client/app/components/host/AddressForm.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import * as M from '../../utils/database/models';
+
+// tslint:disable-next-line:no-var-requires
+const styles = require("./AddressForm.scss");
+
+
+export interface AddressFormProps {
+  address: M.Address;
+  onChange: (address: M.Address) => void;
+}
+
+export class AddressForm extends React.Component<AddressFormProps> {
+  constructor(props: AddressFormProps) {
+    super(props);
+    this.setHost = this.setHost.bind(this);
+    this.setPort = this.setPort.bind(this);
+  }
+
+  public render() {
+    const { host, port } = this.props.address;
+    return (
+      <div>
+        <div className={styles.inputField}>
+            <span>Host</span>
+            <input
+              type="text"
+              value={host}
+              onChange={this.setHost}
+            />
+        </div>
+        <div className={styles.inputField}>
+            <span>Port</span>
+            <input
+              type="number"
+              value={port}
+              onChange={this.setPort}
+            />
+        </div>
+      </div>
+    );
+  }
+
+  private setHost(evt: React.FormEvent<HTMLInputElement>) {
+    this.props.onChange({
+      ...this.props.address,
+      host: evt.currentTarget.value,
+    });
+  }
+
+  private setPort(evt: React.FormEvent<HTMLInputElement>) {
+    this.props.onChange({
+      ...this.props.address,
+      port: evt.currentTarget.valueAsNumber,
+    });
+  }
+}
+
+export default AddressForm;
