095b26e434c3c2b5d7fb740cbbbaec15d07a8569
Ilion Beyst <ilion.beyst@gmail.com>
2018-05-14 18:33:22 +0200

    implement actual request handling

--- gameserver/src/utils/request_handler.rs
+++ gameserver/src/utils/request_handler.rs
@@ -5,9 +5,17 @@ use futures::sync::mpsc::{unbounded, UnboundedSender, UnboundedReceiver};
 use std::io;
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use std::collections::HashMap;
 
 use network::router::RoutingTable;
 use network::connection::Connection;
+
+use utils::timeout_heap::TimeoutHeap;
+
+use prost::Message as ProtobufMessage;
+use bytes::BytesMut;
+use protocol::{self as proto, message};
+
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct ConnectionId {
@@ -15,9 +23,7 @@ pub struct ConnectionId {
 }
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-pub struct MessageId {
-    pub message_number: u64,
-}
+pub struct MessageId(u64);
 
 pub struct Event<RequestData> {
     pub connection_id: ConnectionId,
@@ -64,6 +70,10 @@ pub struct ConnectionHandler<RequestData> {
 
     ctrl_chan: UnboundedReceiver<Command<RequestData>>,
     ctrl_handle: UnboundedSender<Command<RequestData>>,
+
+    requests: HashMap<MessageId, RequestData>,
+    timeouts: TimeoutHeap<MessageId>,
+    message_counter: u64,
     
     event_channel: UnboundedSender<Event<RequestData>>,
 }
@@ -84,6 +94,10 @@ impl<RequestData> ConnectionHandler<RequestData> {
 
             ctrl_chan: rcv,
             ctrl_handle: snd,
+
+            requests: HashMap::new(),
+            timeouts: TimeoutHeap::new(),
+            message_counter: 0,
 
             event_channel,
         };
@@ -116,11 +130,13 @@ impl<RequestData> ConnectionHandler<RequestData> {
         loop {
             match try_ready!(self.poll_ctrl_chan()) {
                 Command::Message { data } => {
-                   self.connection.send(data);
+                    self.send_message(data);
                 },
                 Command::Request { message, request_data, deadline } => {
-                    // TODO
-                }
+                    let message_id = self.send_message(message);
+                    self.requests.insert(message_id, request_data);
+                    self.timeouts.set_timeout(message_id, deadline);
+                },
             }
         }
     }
@@ -135,8 +151,60 @@ impl<RequestData> ConnectionHandler<RequestData> {
         }
     }
  
-    fn handle_client_message(&mut self, msg: Vec<u8>) {
-        // TODO
+    fn handle_client_message(&mut self, data: Vec<u8>) {
+        let message = match proto::Message::decode(data) {
+            Err(_err) => unimplemented!(),
+            Ok(message) => message,
+        };
+
+        match message.payload.unwrap() {
+            message::Payload::Message(message) => {
+                let message_id = MessageId(message.message_id);
+                let data = message.data;
+                let event = EventContent::Message { message_id, data };
+                self.dispatch_event(event);
+            },
+            message::Payload::Response(response) => {
+                let message_id = MessageId(response.message_id);
+                self.resolve_response(message_id, response.data);
+            }
+        }
+    }
+
+    fn resolve_response(&mut self, message_id: MessageId, data: Vec<u8>) {
+        if let Some(request_data) = self.requests.remove(&message_id) {
+            self.timeouts.cancel_timeout(message_id);
+            let value = Ok(data);
+            let event = EventContent::Response { request_data, value };
+            self.dispatch_event(event);
+        }
+    }
+
+    fn send_message(&mut self, data: Vec<u8>) -> MessageId {
+        let message_id = self.message_counter;
+        self.message_counter += 1;
+
+        let message = message::Message { message_id, data };
+        self.send_payload(message::Payload::Message(message));
+        return MessageId(message_id);
+    }
+
+    fn send_reponse(&mut self, message_id: u64, data: Vec<u8>) {
+        let response = message::Response { message_id, data };
+        self.send_payload(message::Payload::Response(response));
+    }
+
+    fn send_payload(&mut self, payload: message::Payload) {
+        let message = proto::Message {
+            payload: Some(payload),
+        };
+
+        let mut bytes = BytesMut::with_capacity(message.encoded_len());
+        // encoding can only fail because the buffer does not have
+        // enough space allocated, but we just allocated the required
+        // space.
+        message.encode(&mut bytes).unwrap();
+        self.connection.send(bytes.to_vec());
     }
 }
 
