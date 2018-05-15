8c384a19b35d12d26b58230e91fa1191eeb40837
Ilion Beyst <ilion.beyst@gmail.com>
2018-05-15 19:30:29 +0200

    update mod.rs

--- gameserver/src/utils/mod.rs
+++ gameserver/src/utils/mod.rs
@@ -1,5 +1,2 @@
-mod player_lock;
-mod message_resolver;
-
-pub use self::message_resolver::{MessageResolver, ResponseValue, ResponseError};
-pub use self::player_lock::{PlayerLock};
+pub mod request_handler;
+pub mod timeout_heap;
