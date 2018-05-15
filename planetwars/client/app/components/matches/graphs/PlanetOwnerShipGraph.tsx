47565cab8ad1b16c1bd0784a9af977274f4f746a
Timo De Waele <timo.dewaele@UGent.be>
2018-05-15 14:33:55 +0200

    stacked bars! don't mind the shitty code

--- planetwars/client/app/components/matches/graphs/PlanetOwnerShipGraph.tsx
+++ planetwars/client/app/components/matches/graphs/PlanetOwnerShipGraph.tsx
@@ -13,6 +13,7 @@ export class PlanetOwnerShipGraphSection extends Section<{}> {
     const log = this.props.log;
     const numberOfStates = log.gameStates.length;
     const planetMap = new Map<string, OwnerShipData>();
+    const planetExpeditionCount = new Map<string, number>();
     log.gameStates.forEach((state) => {
       Object.keys(state.planets).forEach((name) => {
         const planet = state.planets[name];
@@ -31,14 +32,42 @@ export class PlanetOwnerShipGraphSection extends Section<{}> {
           };
           newPlanetOwnerShipData.playerCount.set(planet.owner, 1);
           planetMap.set(planet.name, newPlanetOwnerShipData);
+          const curCount = planetExpeditionCount.get(planet.name);
+          if (curCount !== undefined) {
+            planetExpeditionCount.set(planet.name, curCount + 1);
+          } else {
+            planetExpeditionCount.set(planet.name, 1);
+          }
         }
+      });
+    });
+
+    const planetPlayerData: PlanetPlayerData[] = [];
+    const planetNames = Array.from(planetMap.keys());
+    planetNames.forEach((name) => {
+      const playerCount = (planetMap.get(name) as OwnerShipData).playerCount;
+      Array.from(playerCount.keys()).sort().forEach((player) => {
+        let playerName: string;
+        let playerUuid: string;
+        if (player !== undefined) {
+          playerName = player.name;
+          playerUuid = player.uuid;
+        } else {
+          playerName = "undefined";
+          playerUuid = "undefined";
+        }
+        planetPlayerData.push({ planetName: name,
+          playerName,
+          playerUuid,
+          expeditionCount: (playerCount.get(player) as number),
+        });
       });
     });
 
     const width = 800;
     const height = 400;
 
-    const data: PlanetOwnerShipData = { numberOfStates, planetMap };
+    const data: PlanetOwnerShipData = { numberOfStates, planetPlayerData, planetNames, planetExpeditionCount };
 
     return < PlanetOwnerShipGraph width={width} height={height} data={data} />;
   }
@@ -46,7 +75,16 @@ export class PlanetOwnerShipGraphSection extends Section<{}> {
 
 export interface PlanetOwnerShipData {
   numberOfStates: number;
-  planetMap: Map<string, OwnerShipData>;
+  planetPlayerData: PlanetPlayerData[];
+  planetNames: string[];
+  planetExpeditionCount: Map<string, number>;
+}
+
+export interface PlanetPlayerData {
+  planetName: string;
+  playerName: string;
+  playerUuid: string;
+  expeditionCount: number;
 }
 
 export interface OwnerShipData {
@@ -56,28 +94,39 @@ export interface OwnerShipData {
 export class PlanetOwnerShipGraph extends Graph<PlanetOwnerShipData> {
   protected createGraph(): void { 
     const { width, height, data } = this.props;
-    const planetCount: number = data.planetMap.size;
-    
     const xScale = d3.scaleBand()
-                    .domain(Array.from(data.planetMap.keys()))
+                    .domain(data.planetNames)
                     .rangeRound([0, width])
                     .paddingInner(0.05);
+
+    const heightScale = d3.scaleLinear()
+                    .domain([0, data.numberOfStates])
+                    .range([0, height]);
 
     const node = this.node;
     const svg = d3.select(node);
     svg.selectAll("*").remove();
     const g = svg.append("g");
 
-    g.selectAll("rect")
-      .data(Array.from(data.planetMap.keys()))
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => xScale(d) as number)
-      .attr("y", (d) => 0)
-      .attr("width", xScale.bandwidth)
-      .attr("height", height)
-      .attr("fill", "hotpink");
+    const planetBarCurrentHeight: Map<string, number> = new Map<string, number>();
+    for (let name of data.planetNames) {
+      planetBarCurrentHeight.set(name, 0);
+    }
+
+    data.planetPlayerData.forEach((element) => {
+      const curHeight = heightScale(element.expeditionCount / (data.planetExpeditionCount.get(element.planetName) as number));
+      g.append("rect")
+        .attr("x", xScale(element.planetName) as number)
+        .attr("y", planetBarCurrentHeight.get(element.planetName) as number)
+        .attr("width", xScale.bandwidth)
+        .attr("height", curHeight)
+        .attr("fill", color(element.playerUuid));
+
+      const planetHeight = planetBarCurrentHeight.get(element.planetName) as number;
+      planetBarCurrentHeight.set(element.planetName, planetHeight + curHeight);
+    });
   }
+
 
   protected updateGraph(): void {
     this.createGraph();
