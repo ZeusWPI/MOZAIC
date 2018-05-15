8da2db88ea99b90c99ed46bbbd0508544dc05147
Ilion Beyst <ilion.beyst@gmail.com>
2018-05-15 13:43:10 +0200

    update connection code to emit connect/disconnect events

--- gameserver/src/network/connection.rs
+++ gameserver/src/network/connection.rs
@@ -9,6 +9,74 @@ use super::router::{RoutingTable, RoutingMessage};
 use protobuf_codec::MessageStream;
 use protocol::{Packet, packet};
 
+pub enum ConnectionEvent {
+    Connected,
+    Packet(Vec<u8>),
+    Disconnected,
+}
+
+pub struct ConnectionState {
+    buffer: Vec<packet::Payload>,
+}
+
+impl ConnectionState {
+    fn new() -> Self {
+        ConnectionState {
+            buffer: Vec::new(),
+        }
+    }
+
+    fn queue_send(&mut self, data: Vec<u8>) {
+        let payload = packet::Payload::Message(packet::Message { data });
+        self.buffer.push(payload);
+    }
+
+    fn poll(&mut self, stream: &mut PacketStream)
+        -> Poll<Option<Vec<u8>>, io::Error>
+    {
+        try!(self.flush_buffer(stream));
+        return self.poll_message(stream);
+    }
+
+    fn flush_buffer(&mut self, stream: &mut PacketStream)
+        -> Poll<(), io::Error>
+    {
+        while !self.buffer.is_empty() {
+            try_ready!(stream.poll_complete());
+            let payload = self.buffer.remove(0);
+            let packet = Packet {
+                payload: Some(payload),
+            };
+            let res = try!(stream.start_send(packet));
+            assert!(res.is_ready(), "writing to PacketStream blocked");
+        }
+        return stream.poll_complete();
+    }
+
+    fn poll_message(&mut self, stream: &mut PacketStream)
+        -> Poll<Option<Vec<u8>>, io::Error>
+    {
+        loop {
+            let packet = match try_ready!(stream.poll()) {
+                None => bail!(io::ErrorKind::ConnectionAborted),
+                Some(packet) => packet,
+            };
+
+            if let Some(payload) = packet.payload {
+                match payload {
+                    packet::Payload::Message(message) => {
+                        return Ok(Async::Ready(Some(message.data)))
+                    },
+                    packet::Payload::CloseConnection(_) => {
+                        // TODO
+                        println!("connection closed");
+                    }
+                }
+            }
+        };
+    }
+}
+
 type PacketStream = MessageStream<TcpStream, Packet>;
 
 pub enum StreamState {
@@ -31,7 +99,11 @@ impl StreamHandler {
         self.state = StreamState::Connected(stream);
     }
 
-    fn poll_stream<'a>(&'a mut self) -> Poll<&'a mut PacketStream, io::Error> {
+    fn disconnect(&mut self) {
+        self.state = StreamState::Disconnected;
+    }
+
+    fn poll_stream<'a>(&'a mut self) -> Poll<&'a mut PacketStream, ()> {
         let res = match self.state {
             StreamState::Disconnected => Async::NotReady,
             StreamState::Connected(ref mut stream) => Async::Ready(stream)
@@ -40,13 +112,20 @@ impl StreamHandler {
     }
 }
 
-pub struct Connection {
+pub struct Connection__ {
     /// The token that identifies this connection
     token: Vec<u8>,
     stream_handler: StreamHandler,
     buffer: Vec<packet::Payload>,
     routing_chan: UnboundedReceiver<RoutingMessage>,
 
+}
+
+pub struct Connection {
+    token: Vec<u8>,
+    stream_handler: StreamHandler,
+    state: ConnectionState,
+    routing_chan: UnboundedReceiver<RoutingMessage>,
 }
 
 impl Connection {
@@ -57,67 +136,56 @@ impl Connection {
         Connection {
             token,
             stream_handler: StreamHandler::new(),
-            buffer: Vec::new(),
+            state: ConnectionState::new(),
             routing_chan,
         }
     }
 
     pub fn send(&mut self, data: Vec<u8>) {
-        let payload = packet::Payload::Message(packet::Message { data });
-        self.buffer.push(payload);
+        self.state.queue_send(data);
     }
 
-    pub fn flush_buffer(&mut self) -> Poll<(), io::Error> {
-        self.perform_routing();
-        let stream = try_ready!(self.stream_handler.poll_stream());
-        while !self.buffer.is_empty() {
-            try_ready!(stream.poll_complete());
-            let payload = self.buffer.remove(0);
-            let packet = Packet {
-                payload: Some(payload),
-            };
-            let res = try!(stream.start_send(packet));
-            assert!(res.is_ready(), "writing to PacketStream blocked");
+    // TODO:
+    // Are there any errors that will have to be propagated?
+    // What is the cleanest way to go about this?
+    // How do we handle a closed connection?
+    pub fn poll(&mut self) -> Poll<ConnectionEvent, ()> {
+        match self.poll_routing_chan() {
+            Async::Ready(RoutingMessage::Connecting {stream} ) => {
+                self.stream_handler.connect(stream);
+                // TODO: what is the desired behaviour when there was another
+                // connected stream already?
+                return Ok(Async::Ready(ConnectionEvent::Connected));
+            }
+            Async::NotReady => {}
         }
-        return stream.poll_complete();
-    }
 
-    pub fn poll_message(&mut self) -> Poll<Option<Vec<u8>>, io::Error> {
-        self.perform_routing();
-        let stream = try_ready!(self.stream_handler.poll_stream());
-        loop {
-            let packet = match try_ready!(stream.poll()) {
-                None => bail!(io::ErrorKind::ConnectionAborted),
-                Some(packet) => packet,
-            };
-
-            if let Some(payload) = packet.payload {
-                match payload {
-                    packet::Payload::Message(message) => {
-                        return Ok(Async::Ready(Some(message.data)))
-                    },
-                    packet::Payload::CloseConnection(_) => {
-                        // TODO
-                        println!("connection with token {:?} closed", self.token);
-                    }
-                }
-            }
+        // TODO: can this be extracted into a helper?
+        let res = {
+            let stream = try_ready!(self.stream_handler.poll_stream());
+            self.state.poll(stream)
         };
+
+        match res { 
+            Ok(Async::Ready(item)) => {
+                let msg = item.unwrap(); // TODO
+                return Ok(Async::Ready(ConnectionEvent::Packet(msg)))
+            }
+            Ok(Async::NotReady) => {
+                return Ok(Async::NotReady)
+            }
+            Err(_err) => {
+                self.stream_handler.disconnect();
+                return Ok(Async::Ready(ConnectionEvent::Disconnected));
+            }
+        }
     }
 
-    fn perform_routing(&mut self) {
-        loop {
-            // unwrapping is fine because unbounded channels cannot error
-            let msg = match self.routing_chan.poll().unwrap() {
-                Async::Ready(Some(msg)) => msg,
-                Async::Ready(None) => panic!("router channel closed"),
-                Async::NotReady => return,
-            };
-            match msg {
-                RoutingMessage::Connecting { stream } => {
-                    self.stream_handler.connect(stream);
-                }
-            }
+    fn poll_routing_chan(&mut self) -> Async<RoutingMessage> {
+        match self.routing_chan.poll().unwrap() {
+            Async::Ready(Some(msg)) => Async::Ready(msg),
+            Async::Ready(None) => panic!("router channel closed"),
+            Async::NotReady => Async::NotReady,
         }
     }
 }
