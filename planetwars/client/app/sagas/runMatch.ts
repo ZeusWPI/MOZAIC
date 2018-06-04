bee1ea03d4c2f7eff3151a368b5eec421cbcda99
Ilion Beyst <ilion.beyst@gmail.com>
2018-06-04 16:12:56 +0200

    crappy way of porting the match flow to a saga

--- planetwars/client/app/sagas/runMatch.ts
+++ planetwars/client/app/sagas/runMatch.ts
@@ -1,14 +1,17 @@
 
 import { eventChannel, Channel } from 'redux-saga';
 import * as A from '../actions';
-import { Address, PlayerData, ClientData, PwConfig } from '../reducers/lobby';
+import { Address, PlayerData, ClientData, PwConfig, LobbyState } from '../reducers/lobby';
 import { MatchRunner, MatchControl, Logger, Client, PwClient } from 'mozaic-client';
 import { Config } from '../utils/Config';
 import { generateToken } from '../utils/GameRunner';
 import { ServerParams, BotParams } from '../actions/lobby';
 import { ActionWithPayload } from '../actions/helpers';
 import { ISimpleEvent } from 'ste-simple-events';
+import * as M from '../database/models';
 import { GState } from '../reducers';
+import { parseLogFile, calcStats } from '../lib/match';
+
 import {
   call,
   apply,
@@ -31,7 +34,7 @@ export function* runMatchSaga() {
     const runner: MatchRunner = yield call(startServer, serverParams);
 
     // start the lobby
-    const lobbyTask = yield fork(lobby, runner);
+    const lobbyTask = yield fork(runLobby, runner);
 
     // run lobby until either the server is being shutdown, or the match
     // is being started
@@ -46,7 +49,8 @@ export function* runMatchSaga() {
       yield put(A.serverStopped());
     }
     if (run) {
-      yield spawn(runMatch, runner.matchControl, run.payload);
+      const match = yield call(assembleMatch, serverParams.matchId, run.payload);
+      yield spawn(runMatch, runner, match);
       yield put(A.resetLobby());
     }
   }
@@ -62,20 +66,11 @@ function* startServer(params: ServerParams) {
   return runner;
 }
 
-function* lobby(runner: MatchRunner) {
+function* runLobby(runner: MatchRunner) {
   yield fork(watchConnectEvents, runner.matchControl);
   yield fork(watchDisconnectEvents, runner.matchControl);
   yield fork(watchCreatePlayer, runner.matchControl);
   yield fork(watchRunLocalBot, runner.logger);
-}
-
-function* runMatch(control: MatchControl, conf: PwConfig) {
-  const { mapId, maxTurns } = conf;
-  const mapPath = yield select((state: GState) => state.maps[mapId].mapPath);
-  yield call([control, control.startGame], {
-    map_file: mapPath,
-    max_turns: maxTurns,
-  });
 }
 
 function* watchCreatePlayer(match: MatchControl) {
@@ -133,4 +128,92 @@ function simpleEventChannel<T>(event: ISimpleEvent<T>): Channel<T> {
     const unsubscribe = () => event.unsubscribe(emit);
     return unsubscribe;
   });
+}
+
+function matchEventChannel(runner: MatchRunner) {
+  return eventChannel((emit) => {
+    const completeHandler = () => emit('complete');
+    const errorHandler = (err: Error) => emit(err);
+
+    runner.onComplete.subscribe(completeHandler);
+    runner.onError.subscribe(errorHandler);
+
+    const unsubscribe = () => {
+      runner.onComplete.unsubscribe(completeHandler);
+      runner.onError.unsubscribe(errorHandler);
+    };
+
+    return unsubscribe;
+  });
+}
+
+function* runMatch(runner: MatchRunner, match: M.PlayingHostedMatch) {
+  const mapPath = yield select((state: GState) => state.maps[match.map].mapPath);
+  const matchChan = matchEventChannel(runner);
+
+  yield call([runner.matchControl, runner.matchControl.startGame], {
+    map_file: mapPath,
+    max_turns: match.maxTurns,
+  });
+  yield put(A.saveMatch(match));
+
+  const event = yield take(matchChan);
+  if (event === 'complete') {
+    const log = parseLogFile(match.logPath, match.type);
+    const updatedMatch: M.FinishedMatch = {
+      ...match,
+      stats: calcStats(log),
+      status: M.MatchStatus.finished,
+    };
+    yield put(A.saveMatch(updatedMatch));
+  } else {
+    const updatedMatch: M.ErroredMatch = {
+      ...match,
+      status: M.MatchStatus.error,
+      // TODO: include more information or something
+      error: event.message,
+    };
+    yield put(A.saveMatch(updatedMatch));
+  }
+}
+
+function* assembleMatch(matchId: string, conf: PwConfig) {
+  const lobby: LobbyState = yield select((state: GState) => state.lobby);
+  const players = Object.keys(lobby.players).map((playerId) => {
+    const player = lobby.players[playerId];
+    // just force client id for now
+    const client = lobby.clients[player.clientId!];
+    if (player.botId) {
+      const slot: M.InternalBotSlot = {
+        type: M.BotSlotType.internal,
+        name: player.name,
+        token: client.token,
+        connected: client.connected,
+        botId: player.botId,
+      };
+      return slot;
+    } else {
+      const slot: M.ExternalBotSlot = {
+        type: M.BotSlotType.external,
+        name: player.name,
+        token: client.token,
+        connected: client.connected,
+      };
+      return slot;
+    }
+  });
+
+  const match: M.HostedMatch = {
+    uuid: matchId,
+    type: M.MatchType.hosted,
+    status: M.MatchStatus.playing,
+    maxTurns: conf.maxTurns,
+    map: conf.mapId,
+    timestamp: new Date(),
+    logPath: Config.matchLogPath(matchId),
+    network: lobby.address,
+    players,
+  };
+
+  return match;
 }
