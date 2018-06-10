0528a5e9a8b9ab3479b436a2b81a37c50264cba8
Ilion Beyst <ilion.beyst@gmail.com>
2018-06-11 00:00:23 +0200

    typesafe bots reducer

--- planetwars/client/app/reducers/bots.ts
+++ planetwars/client/app/reducers/bots.ts
@@ -1,38 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import * as A from '../actions';
+import * as actions from '../actions/bots';
 import * as M from '../database/models';
+import { ActionType, getType } from 'typesafe-actions';
 
 export type BotsState = M.BotList;
+export type BotsAction = ActionType<typeof actions>;
 
-export function botsReducer(state: BotsState = {}, action: any) {
-  if (A.addBot.test(action)) {
-    const { name, command } = action.payload;
-    const _now = Date.now();
-    const createdAt = new Date(_now);
-    const lastUpdatedAt = new Date(_now);
-    const uuid = uuidv4();
-    const bot: M.Bot = { uuid, name, command, createdAt, lastUpdatedAt };
-    return { ...state, [bot.uuid]: bot };
+export function botsReducer(state: BotsState = {}, action: BotsAction) {
+  switch (action.type) {
+    case getType(actions.addBot): {
+      const bot = action.payload;
+      return { ...state, [bot.uuid]: bot };
+    }
+    case getType(actions.editBot): {
+      const bot = action.payload;
+      return { ...state, [bot.uuid]: bot };
+    }
+    case getType(actions.removeBot): {
+      const uuid = action.payload;
+      const bots = { ...state };
+      delete bots[uuid];
+      return bots;
+    }
   }
-
-  if (A.importBotFromDB.test(action)) {
-    return { ...state, [action.payload.uuid]: action.payload };
-  }
-
-  if (A.editBot.test(action)) {
-    const bot = action.payload;
-    bot.lastUpdatedAt = new Date(Date.now());
-    state[bot.uuid] = bot;
-    return { ...state };
-  }
-
-  if (A.removeBot.test(action)) {
-    const uuid = action.payload;
-    const bots = { ...state };
-    delete bots[uuid];
-    return bots;
-  }
-
-  return state;
 }
