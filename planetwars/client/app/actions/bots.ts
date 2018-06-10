3856164252e86f2614b8b863f47b28446a798927
Ilion Beyst <ilion.beyst@gmail.com>
2018-06-10 23:51:26 +0200

    typesafe bot actions

--- planetwars/client/app/actions/bots.ts
+++ planetwars/client/app/actions/bots.ts
@@ -1,7 +1,26 @@
-import { actionCreator, actionCreatorVoid } from './helpers';
 import * as M from '../database/models';
+import { v4 as uuidv4 } from 'uuid';
+import { createAction } from 'typesafe-actions';
 
-// Bots
-export const addBot = actionCreator<{ name: string, command: string }>('ADD_BOT');
-export const editBot = actionCreator<M.Bot>('EDIT_BOT');
-export const removeBot = actionCreator<M.BotId>('REMOVE_BOT');
+export const addBot = createAction('ADD_BOT', (resolve) => {
+  return ({ name, command }) => {
+    const now = new Date();
+    const uuid = uuidv4();
+    const bot: M.Bot = {
+      uuid,
+      name,
+      command,
+      createdAt: now,
+      lastUpdatedAt: now,
+    };
+    return resolve(bot);
+  };
+});
+
+export const editBot = createAction('EDIT_BOT', (resolve) => {
+  return (bot: M.Bot) => resolve(bot);
+});
+
+export const removeBot = createAction('REMOVE_BOT', (resolve) => {
+  return (botId: string) => resolve(botId);
+});
