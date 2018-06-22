cfd0667cb168f83c45bf9fb0f154c51c0a20a0ed
Ilion Beyst <ilion.beyst@gmail.com>
2018-06-22 11:04:56 +0200

    add control channels to reactor

--- gameserver/src/utils/reactor.rs
+++ gameserver/src/utils/reactor.rs
@@ -2,6 +2,9 @@ use std::marker::PhantomData;
 use std::any::Any;
 use std::collections::HashMap;
 
+use futures::sync::mpsc;
+use futures::{Future, Poll, Async, Stream};
+
 pub struct Event<T>
     where T: EventType
 {
@@ -31,6 +34,7 @@ pub trait AnyEvent: Any {
 }
 
 pub trait Handler<S> {
+    fn event_type_id(&self) -> u32;
     fn handle_event(&mut self, state: &mut S, event: &AnyEvent);
 }
 
@@ -46,6 +50,10 @@ impl<S, T, F> Handler<S> for EventHandler<S, T, F>
     where F: FnMut(&mut S, &Event<T>),
           T: EventType + 'static
 {
+    fn event_type_id(&self) -> u32 {
+        return T::TYPE_ID;
+    }
+
     fn handle_event(&mut self, state: &mut S, event: &AnyEvent) {
         if let Some(evt) = event.as_any().downcast_ref() {
             (&mut self.handler)(state, evt);
@@ -55,9 +63,18 @@ impl<S, T, F> Handler<S> for EventHandler<S, T, F>
     }
 }
 
+pub enum ReactorCommand<S> {
+    InstallHandler {
+        handler: Box<Handler<S>>,
+    }
+}
+
 pub struct Reactor<S> {
     state: S,
     handlers: HashMap<u32, Box<Handler<S>>>,
+
+    ctrl_chan: mpsc::UnboundedReceiver<ReactorCommand<S>>,
+    event_chan: mpsc::UnboundedReceiver<Box<AnyEvent>>,
 }
 
 impl<S> Reactor<S> {
@@ -65,6 +82,39 @@ impl<S> Reactor<S> {
         let event_type = event.type_id();
         if let Some(handler) = self.handlers.get_mut(&event_type) {
             handler.handle_event(&mut self.state, event);
+        }
+    }
+
+    fn add_handler(&mut self, handler: Box<Handler<S>>) {
+        let type_id = handler.event_type_id();
+        self.handlers.insert(type_id, handler);
+    }
+
+    fn handle_commands(&mut self) -> Poll<(), ()> {
+        loop {
+            match try_ready!(self.ctrl_chan.poll()) {
+                Some(ReactorCommand::InstallHandler { handler }) => {
+                    self.add_handler(handler);
+                }
+                None => {
+                    // TODO: How should this be handled?
+                    return Ok(Async::Ready(()));
+                }
+            }
+        }
+    }
+
+    fn handle_events(&mut self) -> Poll<(), ()> {
+        loop {
+            try!(self.handle_commands());
+            match try_ready!(self.event_chan.poll()) {
+                Some(event) => {
+                    self.handle_event(event.as_ref());
+                }
+                None => {
+                    return Ok(Async::Ready(()));
+                }
+            }
         }
     }
 }
