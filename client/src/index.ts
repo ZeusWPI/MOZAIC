c418a5eefba463be15ff8273bea55970bb30ff92
Ilion Beyst <ilion.beyst@gmail.com>
2018-08-09 21:11:36 +0200

    put PWTypes in its right place

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,5 @@
 export { Address } from './networking/EventWire';
 export { BotRunner, BotConfig } from './planetwars/BotRunner';
 export { PwClient } from './planetwars/PwClient';
+
+export * from './planetwars/PwTypes';
