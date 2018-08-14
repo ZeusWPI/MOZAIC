a19ecc2593c1dee4dc6ce66bfb0aef3d38ffd908
Ilion Beyst <ilion.beyst@gmail.com>
2018-08-14 22:28:48 +0200

    small step towards a functional logger

--- client/src/Logger.ts
+++ client/src/Logger.ts
@@ -1,9 +1,24 @@
 import { WriteStream, createWriteStream } from "fs";
+import * as protocol_root from './proto';
+import { WireEvent } from "./networking/EventWire";
+const { LogEvent } = protocol_root.mozaic.log;
 
 export class Logger {
     writeStream: WriteStream;
+    clientId: number;
 
-    constructor(path: string) {
+    constructor(clientId: number, path: string) {
         this.writeStream = createWriteStream(path);
+        this.clientId = clientId;
+    }
+
+    public log(event: WireEvent) {
+        const logEvent = LogEvent.create({
+            clientId: this.clientId,
+            eventType: event.typeId,
+            data: event.data,
+        });
+        const bytes = LogEvent.encodeDelimited(logEvent).finish();
+        this.writeStream.write(bytes);
     }
 }
