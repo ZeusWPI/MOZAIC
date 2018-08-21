8d4c70b9ea5a0fa66699a3df52d26583e2a9494d
Rien Maertens <maertensrien@gmail.com>
2018-08-21 21:58:09 +0200

    Create test config

--- gameserver/tests/integration_test.rs
+++ gameserver/tests/integration_test.rs
@@ -1,5 +1,12 @@
 
+static LOG_FILE : &str = "not_the_log_file_you_are_looking_for.xml.rar.zip";
+static CTRL_TOKEN : &str = "control-alt-delete";
+static ADDRESS : &str = "127.0.0.1:6969";
+
+
 #[test]
-fn test_add() {
-    assert_eq!(3 + 3, 5);
+fn simple_server_test() {
+    let test_config_json = format!("{{ \"log_file\":\"{}\", \"ctrl_token\":\"{}\", \"address\": \"{}\"}}", LOG_FILE, CTRL_TOKEN, ADDRESS);
+    println!("{}", test_config_json);
+    assert!(false);
 }
