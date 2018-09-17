fac57c0dfc0966483f9fe5728a6046a9ccdc9ea3
Ilion Beyst <ilion.beyst@gmail.com>
2018-09-17 15:46:08 +0200

    implement server challenge

--- gameserver/src/network/handshake.rs
+++ gameserver/src/network/handshake.rs
@@ -9,9 +9,19 @@ use super::tcp::Channel;
 
 use protocol as proto;
 
-use protocol::{SignedMessage, ConnectionRequest};
+use protocol::{
+    HandshakeServerMessage,
+    SignedMessage,
+    ConnectionRequest,
+    ChallengeResponse
+};
+use protocol::handshake_server_message::Payload as HandshakeServerPayload;
 use sodiumoxide::crypto::sign;
 use sodiumoxide::crypto::sign::{PublicKey, Signature};
+use sodiumoxide::randombytes::randombytes;
+use sodiumoxide::utils::memcmp;
+
+const NONCE_NUM_BYTES: usize = 32;
 
 enum Step<State, Next> {
     NotReady(State),
@@ -46,22 +56,34 @@ fn verify_message(message: &SignedMessage, key: &PublicKey) -> bool {
     return sign::verify_detached(&signature, &message.data, key);
 }
 
-fn connection_success() -> proto::ConnectionResponse {
-   proto::ConnectionResponse {
-        response: Some(
-            proto::connection_response::Response::Success(
-                proto::ConnectionSuccess { }
+fn connection_accepted() -> HandshakeServerMessage {
+   HandshakeServerMessage {
+        payload: Some(
+            HandshakeServerPayload::ConnectionAccepted(
+                proto::ConnectionAccepted { }
             )
         )
     }
 }
 
-fn connection_error(msg: String) -> proto::ConnectionResponse {
-    proto::ConnectionResponse {
-        response: Some(
-            proto::connection_response::Response::Error(
-                proto::ConnectionError {
+fn connection_refused(msg: String) -> HandshakeServerMessage {
+    HandshakeServerMessage {
+        payload: Some(
+            HandshakeServerPayload::ConnectionRefused(
+                proto::ConnectionRefused {
                     message: msg,
+                }
+            )
+        )
+    }
+}
+
+fn challenge_msg(nonce: Vec<u8>) -> HandshakeServerMessage {
+    HandshakeServerMessage {
+        payload: Some(
+            HandshakeServerPayload::Challenge(
+                proto::ServerChallenge {
+                    nonce,
                 }
             )
         )
@@ -82,6 +104,7 @@ impl<R: Router> HandshakeState<R> {
     fn step(self) -> Result<Step<Self, Self>, io::Error> {
         match self {
             HandshakeState::Identifying(identifying) => try_step!(identifying),
+            HandshakeState::Challenging(challenging) => try_step!(challenging),
             HandshakeState::Accepting(accepting) => try_step!(accepting),
             HandshakeState::Refusing(refusing) => try_step!(refusing),
             HandshakeState::Done => panic!("stepping done"),
@@ -143,6 +166,7 @@ impl<R: Router> Future for Handshaker<R> {
 
 enum HandshakeState<R: Router> {
     Identifying(Identifying<R>),
+    Challenging(Challenging),
     Refusing(Refusing),
     Accepting(Accepting),
     Done,
@@ -152,7 +176,6 @@ struct Identifying<R: Router> {
     router: ConnectionRouter<R>,
     channel: Channel,
 }
-
 
 impl<R: Router> Identifying<R> {
     fn step(mut self) -> io::Result<Step<Self, HandshakeState<R>>> {
@@ -165,7 +188,7 @@ impl<R: Router> Identifying<R> {
 
         match self.handle_request(frame) {
             Err(err) => {
-                let response = connection_error(err.to_string());
+                let response = connection_refused(err.to_string());
                 try!(self.channel.send_protobuf(response));
 
                 let refusing = Refusing {
@@ -175,14 +198,15 @@ impl<R: Router> Identifying<R> {
 
             }
             Ok(data) => {
-                let response = connection_success();
-                try!(self.channel.send_protobuf(response));
-                let accepting = Accepting {
+                let nonce = randombytes(NONCE_NUM_BYTES);
+                let msg = challenge_msg(nonce.clone());
+                try!(self.channel.send_protobuf(msg));
+                let challenging = Challenging {
+                    server_nonce: nonce,
                     channel: self.channel,
                     connection_data: data,
                 };
-                return Ok(Step::Ready(HandshakeState::Accepting(accepting)));
-            }
+                return Ok(Step::Ready(challenging.into()));            }
         };
     }
 
@@ -209,6 +233,56 @@ impl<R: Router> Identifying<R> {
 impl<R: Router> From<Identifying<R>> for HandshakeState<R> {
     fn from(identifying: Identifying<R>) -> Self {
         HandshakeState::Identifying(identifying)
+    }
+}
+
+struct Challenging {
+    channel: Channel,
+    connection_data: ConnectionData,
+    server_nonce: Vec<u8>,
+}
+
+impl Challenging {
+    fn step<R>(mut self) -> io::Result<Step<Self, HandshakeState<R>>>
+        where R: Router
+    {
+        // make sure channel is ready for writing a response
+        if self.channel.poll_ready().unwrap().is_not_ready() {
+            return Ok(Step::NotReady(self));
+        }
+
+        let frame = try_ready_or!(self, self.channel.poll_frame());
+        let signed_msg = try!(SignedMessage::decode(&frame));
+        if !verify_message(&signed_msg, &self.connection_data.public_key) {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "invalid signature"
+            ));
+        }
+
+        let response = try!(ChallengeResponse::decode(&signed_msg.data));
+
+        if !memcmp(&self.server_nonce, &response.nonce) {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "invalid nonce"
+            ));
+        }
+
+        // succesfully completed challenge, accept connection.
+        let response = connection_accepted();
+        try!(self.channel.send_protobuf(response));
+        let accepting = Accepting {
+            channel: self.channel,
+            connection_data: self.connection_data,
+        };
+        return Ok(Step::Ready(HandshakeState::Accepting(accepting)));
+    }
+}
+
+impl<R: Router> From<Challenging> for HandshakeState<R> {
+    fn from(challenging: Challenging) -> Self {
+        HandshakeState::Challenging(challenging)
     }
 }
 
