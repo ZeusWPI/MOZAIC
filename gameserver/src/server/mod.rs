d1afb0541b793e169b3aa75dedf86827ef6b4b11
Ilion Beyst <ilion.beyst@gmail.com>
2018-09-19 21:42:34 +0200

    PRODUCE AN ACTUAL SPAWN OF SATAN

--- gameserver/src/server/mod.rs
+++ gameserver/src/server/mod.rs
@@ -15,10 +15,7 @@ use tokio;
 use network;
 use network::connection_table::ConnectionTable;
 use network::connection_router::ConnectionRouter;
-use reactors::RequestHandler;
 use sodiumoxide::crypto::sign::{SecretKey, PublicKey};
-
-use self::control_handler::ControlHandler;
 
 
 #[derive(Serialize, Deserialize)]
@@ -48,36 +45,16 @@ impl Future for Server {
     type Error = ();
 
     fn poll(&mut self) -> Poll<(), ()> {
-        let connection_table = Arc::new(Mutex::new(ConnectionTable::new()));
-        let router = Arc::new(Mutex::new(GameServerRouter::new()));
-
-        let connection_manager = ConnectionManager::new(
-            connection_table.clone(),
-            router.clone()
-        );
-
         let control_pubkey = PublicKey::from_slice(&self.config.public_key)
             .expect("invalid public key");
         let secret_key = SecretKey::from_slice(&self.config.private_key)
             .expect("invalid secret key");
 
-        let control_connection = connection_table.lock().unwrap().create(
-            control_pubkey,
-            |handle| {
-                let handler = ControlHandler::new(
-                    handle,
-                    connection_manager.clone(),
-                );
-                let mut core = RequestHandler::new(handler);
-                core.add_handler(ControlHandler::create_match);
-                core.add_handler(ControlHandler::quit);
-                return core;
-            }
+        let connection_table = Arc::new(Mutex::new(ConnectionTable::new()));
+        let router = GameServerRouter::create(
+            connection_table.clone(),
+            control_pubkey
         );
-
-        router.lock()
-            .unwrap()
-            .register_control_connection(control_connection.id());
 
         let addr = self.config.address.parse().unwrap();
         let connection_router = ConnectionRouter {
@@ -85,6 +62,7 @@ impl Future for Server {
             connection_table,
             secret_key,
         };
+
         match network::tcp::Listener::new(&addr, connection_router) {
             Ok(listener) => {
                 tokio::spawn(listener);
--- gameserver/src/server/router.rs
+++ gameserver/src/server/router.rs
@@ -2,28 +2,61 @@ use std::collections::HashMap;
 use prost::Message;
 use protocol as proto;
 use std::io::{Error, ErrorKind};
+use std::sync::{Arc, Weak, Mutex};
 
-use network::connection_router::{Router, Routing};
+use reactors::RequestHandler;
+use network::ConnectionTable;
+use network::connection_router::{Router, Routing, ConnectionCreator};
 
-struct ConnectionData {
-    match_uuid: Vec<u8>,
-    client_id: u32,
+use sodiumoxide::crypto::sign::PublicKey;
+
+use super::control_handler::ControlHandler;
+use super::connection_manager::ConnectionManager;
+
+enum ConnectionData {
+    Client {
+        match_uuid: Vec<u8>,
+        client_id: u32,
+    },
+    Control {
+        uuid: Vec<u8>,
+    },
 }
 
 pub struct GameServerRouter {
-    control_connection: Option<usize>,
+    connection_table: Arc<Mutex<ConnectionTable>>,
+    // TODO: How do we do this in a cleaner fashion?
+    self_reference: Weak<Mutex<GameServerRouter>>,
+
+    owner_public_key: PublicKey,
+
+    /// maps control connection uuid to connection id
+    control_connections: HashMap<Vec<u8>, usize>,
     /// maps match uuid to a (client_id -> connection_id) mapping
     matches: HashMap<Vec<u8>, HashMap<u32, usize>>,
-    client_connections: HashMap<usize, ConnectionData>,
+
+    /// inverse mapping for unregistering
+    connections: HashMap<usize, ConnectionData>,
 }
 
 impl GameServerRouter {
-    pub fn new() -> Self {
-        GameServerRouter {
-            control_connection: None,
+    pub fn create(
+        connection_table: Arc<Mutex<ConnectionTable>>,
+        owner_public_key: PublicKey,
+    ) -> Arc<Mutex<Self>>
+    {
+        let router = GameServerRouter {
+            owner_public_key,
+            connection_table,
+            self_reference: Weak::new(),
+            control_connections: HashMap::new(),
             matches: HashMap::new(),
-            client_connections: HashMap::new(),
-        }
+            connections: HashMap::new(),
+        };
+        // bootstrap self-reference
+        let mutex = Arc::new(Mutex::new(router));
+        mutex.lock().unwrap().self_reference = Arc::downgrade(&mutex);
+        return mutex;
     }
 
     pub fn register_client(
@@ -36,20 +69,25 @@ impl GameServerRouter {
             .entry(match_uuid.clone())
             .or_insert_with(|| HashMap::new());
         match_clients.insert(client_id, connection_id);
-        self.client_connections.insert(connection_id, ConnectionData {
+        self.connections.insert(connection_id, ConnectionData::Client {
             match_uuid,
             client_id,
         });
     }
 
-    pub fn register_control_connection(&mut self, connection_id: usize) {
-        self.control_connection = Some(connection_id);
+    fn register_control(&mut self, uuid: Vec<u8>, connection_id: usize) {
+        self.control_connections.insert(uuid.clone(), connection_id);
+        self.connections.insert(
+            connection_id,
+            ConnectionData::Control { uuid }
+        );
     }
 }
 
 impl Router for GameServerRouter {
     fn route(&self, message: &[u8]) -> Result<Routing<Self>, Error> {
         let connect = try!(proto::GameserverConnect::decode(message));
+
         match connect.connect.unwrap() {
             proto::gameserver_connect::Connect::Client(c) => {
                 self.matches
@@ -63,35 +101,56 @@ impl Router for GameServerRouter {
                         "unknown client")
                     )
             }
-            proto::gameserver_connect::Connect::ServerControl(_) => {
-                self.control_connection
-                    .map(|conn_id| Routing::Connect(conn_id))
-                    .ok_or_else(|| Error::new(
-                        ErrorKind::Other,
-                        "no control connection registered")
-                    )
+            proto::gameserver_connect::Connect::Control(c) => {
+                if let Some(&conn_id) = self.control_connections.get(&c.uuid) {
+                    return Ok(Routing::Connect(conn_id));
+                } else {
+                    // TODO: THIS IS AN ACTUAL SPAWN OF SATAN
 
+                    let creator = ConnectionCreator::new(move |handle, router: &mut GameServerRouter| {            
+                        let conn_manager = ConnectionManager::new(
+                            router.connection_table.clone(),
+                            router.self_reference.upgrade().unwrap(),
+                        );
+
+                        let handler = ControlHandler::new(
+                            handle,
+                            conn_manager,
+                        );
+                        let mut core = RequestHandler::new(handler);
+                        core.add_handler(ControlHandler::create_match);
+                        core.add_handler(ControlHandler::quit);
+                        return core;
+                    });
+
+                    return Ok(Routing::CreateConnection {
+                        public_key: self.owner_public_key.clone(),
+                        creator,
+                    })
+                }
             }
         }
     }
 
     fn unregister(&mut self, connection_id: usize) {
-        if self.control_connection == Some(connection_id) {
-            // this is not really supposed to happen, but sure.
-            self.control_connection = None;
-        }
-
-        if let Some(data) = self.client_connections.remove(&connection_id) {
-            // TODO: ewwwwwwwwww
-            let is_empty = {
-                let match_clients = self.matches
-                    .get_mut(&data.match_uuid)
-                    .unwrap();
-                match_clients.remove(&data.client_id);
-                match_clients.is_empty()
-            };
-            if is_empty {
-                self.matches.remove(&data.match_uuid);
+        if let Some(data) = self.connections.remove(&connection_id) {
+            match data {
+                ConnectionData::Control { uuid } => {
+                    self.control_connections.remove(&uuid);
+                }
+                ConnectionData::Client { match_uuid, client_id } => {
+                    // TODO: ewwwwwwwwww
+                    let is_empty = {
+                        let match_clients = self.matches
+                            .get_mut(&match_uuid)
+                            .unwrap();
+                        match_clients.remove(&client_id);
+                        match_clients.is_empty()
+                    };
+                    if is_empty {
+                        self.matches.remove(&match_uuid);
+                    }
+                }
             }
         }
     }
