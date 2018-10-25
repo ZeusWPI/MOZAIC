c4f7b2b8890f04349a7739f07b17b49e8d8aeab5
Ilion Beyst <ilion.beyst@gmail.com>
2018-10-25 16:22:30 +0200

    draft a cleaner gameserver router implementation

--- gameserver/src/server/router.rs
+++ gameserver/src/server/router.rs
@@ -28,36 +28,47 @@ impl ClientId {
     }
 }
 
-
-enum ConnectionData {
+#[derive(PartialEq, Eq, Hash, Clone)]
+enum GameserverConnection {
+    ServerControl {
+        conn_uuid: Vec<u8>,
+    },
+    MatchControl {
+        match_uuid: Vec<u8>
+    },
     Client {
         match_uuid: Vec<u8>,
         client_id: u32,
-    },
-    Control {
-        uuid: Vec<u8>,
-    },
+    }
 }
+
 
 pub struct GameServerRouter {
     owner_public_key: PublicKey,
 
-    /// maps control connection uuid to connection id
-    control_connections: HashMap<Vec<u8>, usize>,
-    /// maps match uuid to a (client_id -> connection_id) mapping
-    matches: HashMap<Vec<u8>, HashMap<u32, usize>>,
-
+    // map router connection ids to internal connection ids
+    connections: HashMap<GameserverConnection, usize>,
     /// inverse mapping for unregistering
-    connections: HashMap<usize, ConnectionData>,
+    inv: HashMap<usize, GameserverConnection>,
 }
 
 impl GameServerRouter {
     pub fn new(owner_public_key: PublicKey) -> Self {
         GameServerRouter {
             owner_public_key,
-            control_connections: HashMap::new(),
-            matches: HashMap::new(),
             connections: HashMap::new(),
+            inv: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, conn_id: usize, conn: GameserverConnection) {
+        self.connections.insert(conn.clone(), conn_id);
+        self.inv.insert(conn_id, conn);
+    }
+
+    fn remove(&mut self, conn_id: usize) {
+        if let Some(conn) = self.inv.remove(&conn_id) {
+            self.connections.remove(&conn);
         }
     }
 
@@ -67,22 +78,34 @@ impl GameServerRouter {
         client_id: u32,
         connection_id: usize
     ) {
-        let match_clients = self.matches
-            .entry(match_uuid.clone())
-            .or_insert_with(|| HashMap::new());
-        match_clients.insert(client_id, connection_id);
-        self.connections.insert(connection_id, ConnectionData::Client {
+        let router_id = GameserverConnection::Client {
+            match_uuid,
+            client_id,
+        };
+        self.insert(connection_id, GameserverConnection::Client {
             match_uuid,
             client_id,
         });
     }
 
-    fn register_control(&mut self, uuid: Vec<u8>, connection_id: usize) {
-        self.control_connections.insert(uuid.clone(), connection_id);
-        self.connections.insert(
-            connection_id,
-            ConnectionData::Control { uuid }
-        );
+    pub fn register_match_control(
+        &mut self,
+        match_uuid: Vec<u8>,
+        connection_id: usize)
+    {
+        self.insert(connection_id, GameserverConnection::MatchControl {
+            match_uuid,
+        });
+    }
+
+    pub fn register_server_control(
+        &mut self,
+        conn_uuid: Vec<u8>,
+        connection_id: usize
+    ) {
+        self.insert(connection_id, GameserverConnection::ServerControl {
+            conn_uuid,
+        });
     }
 }
 
@@ -92,6 +115,11 @@ impl Router for GameServerRouter {
 
         match connect.connect.unwrap() {
             proto::gameserver_connect::Connect::Client(c) => {
+                let conn_id = GameserverConnection::Client {
+                    match_uuid: c.match_uuid,
+                    client_id: c.client_id,
+                };
+                self.connections.get()
                 self.matches
                     .get(&c.match_uuid)
                     .and_then(|match_clients| {
@@ -117,7 +145,10 @@ impl Router for GameServerRouter {
                         public_key: self.owner_public_key.clone(),
                         spawner: BoxedSpawner::new(
                             move |router: &mut GameServerRouter, conn_id| {
-                                router.register_control(c.uuid.clone(), conn_id);
+                                router.register_server_control(
+                                    c.uuid.clone(),
+                                    conn_id
+                                );
                             },
                             |handle, router| {
                                 let handler = ControlHandler::new(
@@ -137,25 +168,6 @@ impl Router for GameServerRouter {
     }
 
     fn unregister(&mut self, connection_id: usize) {
-        if let Some(data) = self.connections.remove(&connection_id) {
-            match data {
-                ConnectionData::Control { uuid } => {
-                    self.control_connections.remove(&uuid);
-                }
-                ConnectionData::Client { match_uuid, client_id } => {
-                    // TODO: ewwwwwwwwww
-                    let is_empty = {
-                        let match_clients = self.matches
-                            .get_mut(&match_uuid)
-                            .unwrap();
-                        match_clients.remove(&client_id);
-                        match_clients.is_empty()
-                    };
-                    if is_empty {
-                        self.matches.remove(&match_uuid);
-                    }
-                }
-            }
-        }
+        self.remove(connection_id);
     }
 }
