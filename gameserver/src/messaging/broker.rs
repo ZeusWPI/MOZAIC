baceeaa3c2e17387765794863e60fd7572b23923
Ilion Beyst <ilion.beyst@gmail.com>
2018-11-10 21:36:35 +0100

    broker?

--- gameserver/src/messaging/broker.rs
+++ gameserver/src/messaging/broker.rs
@@ -1,8 +1,16 @@
 use std::collections::HashMap;
 use futures::sync::mpsc;
 
+type Uuid = [u8; 16];
+
+#[derive(PartialEq, Eq, Hash)]
+struct ClientId {
+    match_uuid: Uuid,
+    client_num: u64,
+}
+
 //help
 struct Broker {
     recv: mpsc::UnboundedReceiver<()>,
-    actors: HashMap<Vec<u8>, mpsc::UnboundedSender<()>>,
+    actors: HashMap<ClientId, mpsc::UnboundedSender<()>>,
 }
