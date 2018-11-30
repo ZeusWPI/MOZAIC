f43ac0650b05c5dc6512a9ef029fff1919c5375d
Ilion Beyst <ilion.beyst@gmail.com>
2018-11-30 22:11:24 +0100

    outline a test setup

--- gameserver/src/messaging/mod.rs
+++ gameserver/src/messaging/mod.rs
@@ -9,6 +9,7 @@ use std::marker::PhantomData;
 
 pub mod broker;
 pub mod reactor;
+pub mod test;
 
 pub struct HandlerSet<S, T, E> {
     handler_map: HashMap<u64, BoxedHandler<S, T, E>>,
--- gameserver/src/messaging/test.rs
+++ gameserver/src/messaging/test.rs
@@ -1,4 +1,27 @@
+use std::collections::{VecDeque, HashMap};
+
 use super::broker::Broker;
+use super::reactor::{Uuid, Reactor};
+
+use futures::sync::mpsc;
 
 pub fn run() {
+    let (broker_snd, broker_recv) = mpsc::unbounded();
+    let (reactor_snd, reactor_recv) = mpsc::unbounded();
+
+    let reactor = Reactor {
+        uuid: Uuid {
+            x0: 8,
+            x1: 8,
+        },
+        message_chan: reactor_recv,
+        broker_handle: broker_snd,
+        message_queue: VecDequeue::new(),
+        internal_state: CoreState,
+        internal_handlers: HashMap::new(),
+        links: HashMap::new(),
+    }
 }
+
+
+struct CoreState {}
