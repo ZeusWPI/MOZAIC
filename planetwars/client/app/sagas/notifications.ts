78fc126b28adba1b79ae8cc7e683f08cdada75c6
Wout Schellaert <wout.schellaert@gmail.com>
2019-02-02 17:23:06 +0100

    Add some documentation (and more verbose variable names)

--- planetwars/client/app/sagas/notifications.ts
+++ planetwars/client/app/sagas/notifications.ts
@@ -1,5 +1,6 @@
-import * as A from '../actions';
 import { takeEvery, put, fork } from 'redux-saga/effects';
+
+import * as A from '../actions';
 
 export function* runNotificationSaga() {
   yield fork(watchMatchFinished);
@@ -7,7 +8,7 @@ export function* runNotificationSaga() {
 }
 
 function* watchMatchFinished() {
-  yield takeEvery(A.matchFinished.type, function*(action: any) {
+  yield takeEvery(A.matchFinished.type, function* (action: any) {
     const { matchId } = action.payload;
     const title = 'Match ended';
     const body = `A match has ended`;
@@ -17,10 +18,10 @@ function* watchMatchFinished() {
 }
 
 function* watchMatchError() {
-  yield takeEvery(A.matchError.type, function*(action: any) {
+  yield takeEvery(A.matchError.type, function* (action: any) {
     const { matchId } = action.payload;
-    const title = 'Match errored';
-    const body = `A match has errored`;
+    const title = 'Match errorred';
+    const body = `A match has errorred`;
     const link = `/matches/${matchId}`;
     yield put(A.addNotification({ title, body, link, type: 'Error' }));
   });
