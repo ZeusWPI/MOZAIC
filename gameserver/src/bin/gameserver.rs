9368746de3ba85e4cfe2417eb80082e6331ef2a1
Ilion Beyst <ilion.beyst@gmail.com>
2019-02-12 10:28:07 +0100

    stub gameserver binary

--- gameserver/src/bin/gameserver.rs
+++ gameserver/src/bin/gameserver.rs
@@ -9,7 +9,7 @@ extern crate serde_json;
 extern crate tokio;
 extern crate mozaic;
 
-use mozaic::server::{Config as ServerConfig, Server};
+// use mozaic::server::{Config as ServerConfig, Server};
 
 // Load the config and start the game.
 fn main() {
@@ -18,39 +18,41 @@ fn main() {
 
 pub fn run(args : Vec<String>) {
     sodiumoxide::init().expect("failed to initialize libsodium");
-    if args.len() != 2 {
-        println!("Expected 1 argument (config file). {} given.", args.len() - 1);
-        std::process::exit(1)
-    }
+    mozaic::messaging::test::run();
+    // if args.len() != 2 {
+    //     println!("Expected 1 argument (config file). {} given.", args.len() - 1);
+    //     std::process::exit(1)
+    // }
 
-    let config = match parse_config(Path::new(&args[1])) {
-        Ok(config) => config,
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(1)
-        }
-    };
+    // let config = match parse_config(Path::new(&args[1])) {
+    //     Ok(config) => config,
+    //     Err(e) => {
+    //         println!("{}", e);
+    //         std::process::exit(1)
+    //     }
+    // };
 
-    let server = Server::new(config);
-    tokio::run(server);
+    // let server = Server::new(config);
+    // tokio::run(server);
 }
 
 
 // Parse a config passed to the program as an command-line argument.
 // Return the parsed config.
-fn parse_config(path: &Path)
-    -> Result<ServerConfig, Box<Error>>
-{
-    println!("Opening config {}", path.to_str().unwrap());
-    let mut file = File::open(path)?;
 
-    println!("Reading contents");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+// fn parse_config(path: &Path)
+//     -> Result<ServerConfig, Box<Error>>
+// {
+//     println!("Opening config {}", path.to_str().unwrap());
+//     let mut file = File::open(path)?;
 
-    println!("Parsing config");
-    let config: ServerConfig = serde_json::from_str(&contents)?;
+//     println!("Reading contents");
+//     let mut contents = String::new();
+//     file.read_to_string(&mut contents)?;
 
-    println!("Config parsed succesfully");
-    Ok(config)
-}
+//     println!("Parsing config");
+//     let config: ServerConfig = serde_json::from_str(&contents)?;
+
+//     println!("Config parsed succesfully");
+//     Ok(config)
+// }
