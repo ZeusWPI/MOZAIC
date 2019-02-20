1aa51b170390d62d2f4175eb38ec98614febac3f
Ilion Beyst <ilion.beyst@gmail.com>
2019-02-20 10:30:43 +0100

    impl welcomer

--- gameserver/src/bin/server.rs
+++ gameserver/src/bin/server.rs
@@ -15,11 +15,14 @@ extern crate rand;
 
 use std::net::SocketAddr;
 use tokio::prelude::Stream;
+use tokio::runtime::Runtime;
 use futures::sync::mpsc;
 use mozaic::net::{StreamHandler, Writer, MsgHandler, Forwarder};
 use mozaic::network_capnp::{connect, connected};
-use mozaic::messaging::types::{Uuid, Message, set_uuid};
+use mozaic::core_capnp::{initialize, actor_joined};
 use mozaic::messaging::runtime::{Broker, BrokerHandle};
+use mozaic::messaging::types::*;
+use mozaic::messaging::reactor::*;
 
 use rand::Rng;
 
@@ -38,8 +41,14 @@ impl ConnectionHandler {
     fn handle_connect(&mut self, w: &mut Writer, r: connect::Reader)
         -> Result<(), capnp::Error>
     {
-        println!("got connect");
-        self.broker.register(r.get_id()?.into(), self.tx.clone());
+        let connecting_id: Uuid = r.get_id()?.into();
+        self.broker.register(connecting_id.clone(), self.tx.clone());
+
+        self.broker.send_message(&self.welcomer_id, actor_joined::Owned, |b| {
+            let mut joined: actor_joined::Builder = b.init_as();
+            set_uuid(joined.reborrow().init_id(), &connecting_id);
+        });
+
         w.write(connected::Owned, |b| {
             let mut connected: connected::Builder = b.init_as();
             set_uuid(connected.reborrow().init_id(), &self.welcomer_id);
@@ -48,12 +57,69 @@ impl ConnectionHandler {
     }
 }
 
+struct Welcomer {
+    runtime_id: Uuid,
+}
+
+impl Welcomer {
+    fn params<C: Ctx>(self) -> CoreParams<Self, C> {
+        let mut params = CoreParams::new(self);
+        params.handler(initialize::Owned, CtxHandler::new(Self::initialize));
+        return params;
+    }
+
+    fn initialize<C: Ctx>(
+        &mut self,
+        handle: &mut ReactorHandle<C>,
+        _: initialize::Reader,
+    ) -> Result<(), capnp::Error>
+    {
+        let link = WelcomerRuntimeLink {};
+        handle.open_link(link.params(self.runtime_id.clone()));
+        return Ok(());
+    }
+}
+
+struct WelcomerRuntimeLink {}
+
+impl WelcomerRuntimeLink {
+    fn params<C: Ctx>(self, foreign_uuid: Uuid) -> LinkParams<Self, C> {
+        let mut params = LinkParams::new(foreign_uuid, self);
+        params.external_handler(
+            actor_joined::Owned,
+            CtxHandler::new(Self::welcome),
+        );
+
+        return params;
+    }
+
+    fn welcome<C: Ctx>(
+        &mut self,
+        handle: &mut LinkHandle<C>,
+        r: actor_joined::Reader,
+    ) -> Result<(), capnp::Error>
+    {
+        let id: Uuid = r.get_id()?.into();
+        println!("welcoming {:?}", id);
+        return Ok(());
+    }
+}
+
 pub fn run(_args : Vec<String>) {
+
     let addr = "127.0.0.1:9142".parse::<SocketAddr>().unwrap();
     let listener = tokio::net::TcpListener::bind(&addr).unwrap();
-    let broker = Broker::new();
+    let mut broker = Broker::new();
 
-    tokio::run(listener.incoming()
+    tokio::run(futures::lazy(move || {
+        let welcomer_id: Uuid = rand::thread_rng().gen();
+        let welcomer = Welcomer {
+            runtime_id: broker.get_runtime_id(),
+        };
+
+        broker.spawn(welcomer_id.clone(), welcomer.params());
+
+        listener.incoming()
         .map_err(|e| eprintln!("failed to accept socket; error = {:?}", e))
         .for_each(move |stream| {
             println!("got connection");
@@ -61,7 +127,7 @@ pub fn run(_args : Vec<String>) {
 
             let state = ConnectionHandler {
                 broker: broker.clone(),
-                welcomer_id: rand::thread_rng().gen(),
+                welcomer_id: welcomer_id.clone(),
                 tx,
             };
             let mut handler = mozaic::net::StreamHandler::new(state, stream);
@@ -69,7 +135,7 @@ pub fn run(_args : Vec<String>) {
                 MsgHandler::new(ConnectionHandler::handle_connect));
             Forwarder { handler, rx }
         })
-    );
+    }));
 
     // if args.len() != 2 {
     //     println!("Expected 1 argument (config file). {} given.", args.len() - 1);
